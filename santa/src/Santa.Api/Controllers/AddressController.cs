ec996cbf860497ad46695e14f78f2347111cef0c
Memely <memelypepeartly@gmail.com>
2019-12-09 15:16:09 -0600

    Project bones

---
+++ santa/src/Santa.Api/Controllers/AddressController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Santa.Api.Controllers
+{
+    /// <summary>
+    /// RESTful API for addresses
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        // GET: api/Address
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Address/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Address
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Address/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
