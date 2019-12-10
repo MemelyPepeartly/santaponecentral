87a4a83d6ba8c5faaa5826f4880d259b75882377
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-10 01:57:19 -0600

    splash updates

--- santa/AngularClient/santa-pone-central/src/app/splash/splash.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/splash/splash.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-splash',
@@ -11,5 +12,5 @@ export class SplashComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
+
