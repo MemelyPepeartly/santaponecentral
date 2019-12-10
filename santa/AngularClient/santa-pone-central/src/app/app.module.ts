9494753f30c1887df5415b2139e6e6335c050b3c
Memely <memelypepeartly@gmail.com>
2019-12-10 09:52:04 -0600

    Bug fixes. Clearning up module implimentation

--- santa/AngularClient/santa-pone-central/src/app/app.module.ts
+++ santa/AngularClient/santa-pone-central/src/app/app.module.ts
@@ -7,21 +7,13 @@ import { SplashComponent } from './splash/splash.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SantaponecontrolComponent } from './santaponecontrol/santaponecontrol.component';
 import { NotificationsComponent } from './notifications/notifications.component';
-import { MatMenuModule } from '@angular/material/menu';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { Routes, RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 import { SantaponenavComponent } from './navbar/santaponenav/santaponenav.component';
 import { UsernavComponent } from './navbar/usernav/usernav.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { LoginComponent } from './splash/login/login.component';
 import { SignupComponent } from './splash/signup/signup.component';
 import { InformationComponent } from './splash/information/information.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
@@ -51,14 +43,6 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatMenuModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatTabsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
--- santa/AngularClient/santa-pone-central/src/app/splash/login/login.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/splash/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
   }
+  @Input() error: string | null;
 
+  @Output() submitEM = new EventEmitter();
 }
