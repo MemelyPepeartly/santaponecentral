1a177a374e7b77173f3665f4d02b302c58792685
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-10 10:02:35 -0600

    cleanup and import fixes

--- santa/AngularClient/santa-pone-central/src/app/app.module.ts
+++ santa/AngularClient/santa-pone-central/src/app/app.module.ts
@@ -1,19 +1,26 @@
+//Necessary
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Extras
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './core/material.module';
+import { FormsModule } from '@angular/forms';
+
+//Componenents
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SplashComponent } from './splash/splash.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SantaponecontrolComponent } from './santaponecontrol/santaponecontrol.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { Routes, RouterModule } from '@angular/router';
 import { SantaponenavComponent } from './navbar/santaponenav/santaponenav.component';
 import { UsernavComponent } from './navbar/usernav/usernav.component';
 import { LoginComponent } from './splash/login/login.component';
 import { SignupComponent } from './splash/signup/signup.component';
 import { InformationComponent } from './splash/information/information.component';
+
 
 const appRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
@@ -43,6 +50,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
