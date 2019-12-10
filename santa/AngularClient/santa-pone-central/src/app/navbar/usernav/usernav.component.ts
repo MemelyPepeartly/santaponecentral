27cb2912684efd6cf7c37fa0a619aa7d1dbecc2f
Memely <memelypepeartly@gmail.com>
2019-12-09 18:49:36 -0600

    Started modifying navbars and splash

---
+++ santa/AngularClient/santa-pone-central/src/app/navbar/usernav/usernav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usernav',
+  templateUrl: './usernav.component.html',
+  styleUrls: ['./usernav.component.css']
+})
+export class UsernavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
