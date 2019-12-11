90473a1d1736e2ca03ddecf2cc3256661e93084c
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-11 10:53:50 -0600

    new componenets and nav

---
+++ santa/AngularClient/santa-pone-central/src/app/navbar/defaultnav/defaultnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defaultnav',
+  templateUrl: './defaultnav.component.html',
+  styleUrls: ['./defaultnav.component.css']
+})
+export class DefaultnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
