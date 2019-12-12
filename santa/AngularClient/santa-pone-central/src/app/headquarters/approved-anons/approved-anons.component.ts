1a66b87a7916ef35061fc0d8dd6f58da1972ceca
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-12 09:57:01 -0600

    headquarters componenets added

---
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/approved-anons/approved-anons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-approved-anons',
+  templateUrl: './approved-anons.component.html',
+  styleUrls: ['./approved-anons.component.css']
+})
+export class ApprovedAnonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/headquarters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headquarters',
+  templateUrl: './headquarters.component.html',
+  styleUrls: ['./headquarters.component.css']
+})
+export class HeadquartersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
