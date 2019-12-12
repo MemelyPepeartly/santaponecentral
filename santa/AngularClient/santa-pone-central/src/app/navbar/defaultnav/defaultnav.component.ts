f613e154c9a729339257487d6053b95445b9f805
RAINBOOMERS-PC <memelypeperartly@gmail.com>
2019-12-11 20:37:24 -0600

    I lied

--- santa/AngularClient/santa-pone-central/src/app/navbar/defaultnav/defaultnav.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/navbar/defaultnav/defaultnav.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { LoginComponent } from '../../home/login/login.component';
 
-export interface DialogData {
-  username: string;
-  password: string;
-}
 
 @Component({
   selector: 'app-defaultnav',
@@ -16,30 +12,11 @@ export class DefaultnavComponent implements OnInit {
 
   username = '';
   password = '';
-  constructor( public dialog: MatDialog) { }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(LoginComponent, {
-      width: '250px',
-      data: {user: this.username, pass: this.password}
-    });
-  }
+
+  constructor( ) { }
+
 
   ngOnInit() {
   }
-
 }
-@Component({
-  selector: 'app-dialog-login',
-  templateUrl: '../../home/login/login.component.html',
-  styleUrls: ['../../home/login/login.component.css']
-})
-export class LogindialogComponent {
 
-  constructor(
-    public dialogRef: MatDialogRef<LogindialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-}
