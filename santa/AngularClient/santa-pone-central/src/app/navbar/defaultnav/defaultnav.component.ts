ee39b97c3ff9a7a1bf4a87723e26f1776d35866b
RAINBOOMERS-PC <memelypeperartly@gmail.com>
2019-12-11 20:26:54 -0600

    added login dialogue. Still buggy

--- santa/AngularClient/santa-pone-central/src/app/navbar/defaultnav/defaultnav.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/navbar/defaultnav/defaultnav.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { LoginComponent } from '../../home/login/login.component';
+
+export interface DialogData {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-defaultnav',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultnavComponent implements OnInit {
 
-  constructor() { }
+  username = '';
+  password = '';
+  constructor( public dialog: MatDialog) { }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '250px',
+      data: {user: this.username, pass: this.password}
+    });
+  }
 
   ngOnInit() {
   }
 
+}
+@Component({
+  selector: 'app-dialog-login',
+  templateUrl: '../../home/login/login.component.html',
+  styleUrls: ['../../home/login/login.component.css']
+})
+export class LogindialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<LogindialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
