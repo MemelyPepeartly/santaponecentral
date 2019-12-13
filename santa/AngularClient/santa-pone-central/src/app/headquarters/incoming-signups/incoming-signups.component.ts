e59fc1da0f7c79f9b4d3a27f3a576a129c4febce
TheMemelyMan <memelypeperartly@gmail.com>
2019-12-12 20:08:07 -0600

    styling directive

--- santa/AngularClient/santa-pone-central/src/app/headquarters/incoming-signups/incoming-signups.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/incoming-signups/incoming-signups.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Anon } from '../../../interfaces/anon';
 import { Address } from '../../../interfaces/address';
-import { emit } from 'cluster';
+import { MaterialElevationDirective } from '../material-elevation.directive';
 
 @Component({
   selector: 'app-incoming-signups',
@@ -10,18 +10,35 @@ import { emit } from 'cluster';
 })
 export class IncomingSignupsComponent implements OnInit {
 
+  defaultElevation = 2;
+  raisedElevation = 8;
+
   constructor() { }
 
-  address: Address = {streetAddress: '123 Test Street',
+  // test data
+  
+  address: Address = 
+  {
+    streetAddress: '123 Test Street',
     city: 'TestCity',
     state: 'TS',
     zipcode: '12345'
   };
-  requests: Anon[] = [{realName: 'Steve',
+  requests: Anon[] = [
+  {
+  realName: 'Zoomer',
   address: this.address,
   wishlist: 'I want ponies!',
   email: 'anon@anon.non',
-  otherInfo: 'Im allergic to normies'}
+  otherInfo: 'Im allergic to normies'
+  },
+  {
+  realName: 'Wojak',
+  address: this.address,
+  wishlist: 'I want to die!',
+  email: 'wojak@anon.non',
+  otherInfo: 'Im allergic to happiness'
+  },
   ];
 
   ngOnInit() {
---
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/material-elevation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MaterialElevationDirective } from './material-elevation.directive';
+
+describe('MaterialElevationDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MaterialElevationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
