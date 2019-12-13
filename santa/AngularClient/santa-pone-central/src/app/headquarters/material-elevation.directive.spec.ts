424cd15e138144788d6463cfd42ffa0532e9ae65
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-12 20:18:38 -0600

    removed spec on directive hover

--- santa/AngularClient/santa-pone-central/src/app/headquarters/material-elevation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MaterialElevationDirective } from './material-elevation.directive';
-
-describe('MaterialElevationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MaterialElevationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
