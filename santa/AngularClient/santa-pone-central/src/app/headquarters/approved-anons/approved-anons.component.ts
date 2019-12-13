517823fe104ccb618f7bb09ebf9d69660a3c38e2
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-12 21:11:04 -0600

    styling and test data

--- santa/AngularClient/santa-pone-central/src/app/headquarters/approved-anons/approved-anons.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/approved-anons/approved-anons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Anon } from '../../../interfaces/anon';
+import { Address } from '../../../interfaces/address';
 
 @Component({
   selector: 'app-approved-anons',
@@ -8,6 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class ApprovedAnonsComponent implements OnInit {
 
   constructor() { }
+
+    // test data
+  
+    address: Address = 
+    {
+      streetAddress: '123 Test Street',
+      city: 'TestCity',
+      state: 'TS',
+      zipcode: '12345'
+    };
+    santas: Anon[] = [
+    {
+    realName: 'Pepe',
+    address: this.address,
+    wishlist: 'I want to feel!',
+    email: 'pepe@anon.non',
+    otherInfo: 'REEEEEEEEE'
+    },
+    {
+    realName: 'Virgin',
+    address: this.address,
+    wishlist: 'I dont want much...',
+    email: 'virgin@anon.non',
+    otherInfo: 'Im... er... nevermind...'
+    },
+    ];
 
   ngOnInit() {
   }
