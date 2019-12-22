2a53af9589b6143dfb38ce4806a39a285dcdeb32
TheMemelyMan <memelypepeartly@gmail.com>
2019-12-21 22:02:13 -0600

    working general component with test data

--- santa/AngularClient/santa-pone-central/src/app/headquarters/incoming-correspondence/general-correspondence/general-correspondence.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/incoming-correspondence/general-correspondence/general-correspondence.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../../../../interfaces/message';
+import { Address } from '../../../../interfaces/address';
+import { Anon } from '../../../../interfaces/anon';
 
 @Component({
   selector: 'app-general-correspondence',
@@ -8,6 +11,45 @@ import { Component, OnInit } from '@angular/core';
 export class GeneralCorrespondenceComponent implements OnInit {
 
   constructor() { }
+
+  //test data
+
+  address: Address = 
+  {
+    streetAddress: '123 Test Street',
+    city: 'TestCity',
+    state: 'TS',
+    zipcode: '12345'
+  };
+  anons: Anon[] = [
+  {
+  realName: 'Zoomer',
+  holidayID: 'Zippy Zoomer',
+  address: this.address,
+  wishlist: 'I want ponies!',
+  email: 'anon@anon.non',
+  otherInfo: 'Im allergic to normies'
+  },
+  {
+  realName: 'Wojak',
+  holidayID: 'Wilin Wojak',
+  address: this.address,
+  wishlist: 'I want to die!',
+  email: 'wojak@anon.non',
+  otherInfo: 'Im allergic to happiness'
+  }];
+
+  messages: Message[] = [
+  {
+    anonInfo: this.anons[0],
+    subject: 'Question',
+    message: 'Hi, I just had a question',
+  },
+  {
+    anonInfo: this.anons[1],
+    subject: 'Santee question',
+    message: 'I dunno if Imma be grinched, Ill be real'
+  }];
 
   ngOnInit() {
   }
