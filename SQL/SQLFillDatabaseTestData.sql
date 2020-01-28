7ce5aa3fc2638b7de4bf608531a812b73ca1ccd9
TheMemelyMan <memelypeperartly@gmail.com>
2020-01-27 23:51:47 -0500

    Database planning and setup queries started

---
+++ SQL/SQLFillDatabaseTestData.sql
@@ -0,0 +1,30 @@
+INSERT INTO app.ClientRelationXref
+VALUES
+    ();
+INSERT INTO app.ClientStatus
+VALUES
+    ();
+INSERT INTO app.Client
+VALUES
+    ();
+INSERT INTO app.SurveyResponse
+VALUES
+    ();
+INSERT INTO app.EventType
+VALUES
+    ();
+INSERT INTO app.SurveyQuestionOptionXref
+VALUES
+    ();
+INSERT INTO app.SurveyOption
+VALUES
+    ();
+INSERT INTO app.Survey
+VALUES
+    ();
+INSERT INTO app.SurveyOptionXref
+VALUES
+    ();
+INSERT INTO app.SurveyQuestion
+VALUES
+    ();
