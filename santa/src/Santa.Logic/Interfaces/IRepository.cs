ed2a3d95acc265ef21c166145040a91f5b18bd64
TheMemelyMan <memelypeperartly@gmail.com>
2020-02-02 20:21:50 -0500

    Basic repo pattern setup

---
+++ santa/src/Santa.Logic/Interfaces/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Santa.Logic.Objects;
+
+namespace Santa.Logic.Interfaces
+{
+    public interface IRepository
+    {
+        #region Client
+        Task<Logic.Objects.Client> CreateClientAsync();
+        Task<Logic.Objects.Client> GetClientByID();
+        Task<Logic.Objects.Client> GetClientByEmailAsync();
+        Task<Logic.Objects.Client> UpdateClientByIDAsync();
+        Task<Logic.Objects.Client> DeleteClientByIDAsync();
+        #endregion
+
+        #region Event
+
+        #endregion
+
+        #region Survey
+        #endregion
+    }
+}
---
+++ santa/src/Santa.Logic/Objects/Survey.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    class Survey
+    {
+    }
+}
