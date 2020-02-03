1a288ac5fd2a43a6cd8c2687e61b3f4ba53c3811
TheMemelyMan <memelypepeartly@gmail.com>
2020-02-02 20:21:50 -0500

    Basic repo pattern setup

---
+++ santa/src/Santa.Logic/Objects/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class Client
+    {
+        public string nickname { get; set; }
+        public string email { get; set; }
+        public Guid clientID { get; set; }
+        public List<Logic.Objects.Client> givers { get; set; } = new List<Client>();
+        public List<Logic.Objects.Client> recievers { get; set; } = new List<Client>();
+
+        //Constructor
+        public Client(string _nickname, string _email)
+        {
+            nickname = _nickname;
+            email = _email;
+        }
+
+
+    }
+}
