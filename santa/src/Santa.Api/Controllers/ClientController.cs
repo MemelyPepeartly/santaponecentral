ea0b28a67febddd134717284616a56d857424eda
TheMemelyMan <memelypeperartly@gmail.com>
2020-02-05 00:29:12 -0500

    wrong api controller type, whoops

--- santa/src/Santa.Api/Controllers/ClientController.cs
+++ santa/src/Santa.Api/Controllers/ClientController.cs
@@ -3,111 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Santa.Logic.Interfaces;
-using Newtonsoft.Json;
 
 namespace Santa.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ClientController : Controller
+    public class ClientController : ControllerBase
     {
         private readonly IRepository repository;
-
         public ClientController(IRepository _repository)
         {
             repository = _repository;
         }
         // GET: api/Client
         [HttpGet]
-        public ActionResult<object> GetAllClients()
+        public IEnumerable<string> Get()
         {
-            try
-            {
-                List<Logic.Objects.Client> clients = repository.GetAllClients();
-                return Ok(clients);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-            }
+            return new string[] { "value1", "value2" };
         }
 
-        // GET: Client/Details/5
-        public ActionResult Details(int id)
+        // GET: api/Client/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
-            return View();
+            return "value";
         }
 
-        // GET: Client/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Client/Create
+        // POST: api/Client
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public void Post([FromBody] string value)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
 
-        // GET: Client/Edit/5
-        public ActionResult Edit(int id)
+        // PUT: api/Client/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
         {
-            return View();
         }
 
-        // POST: Client/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
         {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Client/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Client/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
