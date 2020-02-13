3d13f33d2bf3513b43bb4bdf7fbd52caca1939d0
TheMemelyMan <memelypepeartly@gmail.com>
2020-02-13 17:48:35 -0500

    Inserting mock data into DB for testing. Updated SQL entries

---
+++ SQL/SQLDataBaseDrop.sql
@@ -0,0 +1,15 @@
+DROP TABLE app.SurveyResponse;
+DROP TABLE app.SurveyQuestionOptionXref;
+DROP TABLE app.SurveyOption;
+
+DROP TABLE app.SurveyQuestionXref;
+DROP TABLE app.SurveyQuestion;
+DROP TABLE app.Survey;
+
+DROP TABLE app.ClientRelationXref;
+DROP TABLE app.Client;
+DROP TABLE app.ClientStatus;
+
+DROP TABLE app.EventType;
+
+DROP SCHEMA app;
