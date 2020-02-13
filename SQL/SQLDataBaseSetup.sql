fe85669502ae4d71307698fce067d4d13882b132
TheMemelyMan <memelypeperartly@gmail.com>
2020-02-13 17:48:35 -0500

    Inserting mock data into DB for testing. Updated SQL entries

--- SQL/SQLDataBaseSetup.sql
+++ SQL/SQLDataBaseSetup.sql
@@ -3,27 +3,27 @@ GO
 
 CREATE TABLE app.ClientStatus
 (
-    clientStatusID INT PRIMARY KEY,
+    clientStatusID UNIQUEIDENTIFIER PRIMARY KEY,
     statusDescription NVARCHAR(25) NOT NULL
 );
 
 CREATE TABLE app.Client
 (
     clientID UNIQUEIDENTIFIER PRIMARY KEY,
-    clientStatusID INT FOREIGN KEY REFERENCES app.ClientStatus(clientStatusID) NOT NULL,
+    clientStatusID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.ClientStatus(clientStatusID) NOT NULL,
     clientName NVARCHAR(50) NOT NULL,
     nickname NVARCHAR(50) NOT NULL UNIQUE,
     email NVARCHAR(50) NOT NULL UNIQUE,
     addressLine1 NVARCHAR(50) NOT NULL,
     addressLine2 NVARCHAR(50),
-    city NVARCHAR(25) NOT NULL,
-    state NVARCHAR(25) NOT NULL,
-    postalCode NVARCHAR(10) NOT NULL,
+    city NVARCHAR(50) NOT NULL,
+    [state] NVARCHAR(50) NOT NULL,
+    postalCode NVARCHAR(25) NOT NULL,
     country NVARCHAR(50) NOT NULL
 );
 CREATE TABLE app.EventType
 (
-    eventTypeID INT PRIMARY KEY,
+    eventTypeID UNIQUEIDENTIFIER PRIMARY KEY,
     eventDescription NVARCHAR(100) NOT NULL,
     isActive BIT NOT NULL
 
@@ -32,20 +32,20 @@ CREATE TABLE app.ClientRelationXref
 (
     senderClientID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.Client(clientID) NOT NULL,
     recipientClientID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.Client(clientID) NOT NULL,
-    eventTypeID INT FOREIGN KEY REFERENCES app.EventType(eventTypeID) NOT NULL
+    eventTypeID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.EventType(eventTypeID) NOT NULL
 );
 CREATE TABLE app.Survey
 (
     surveyID UNIQUEIDENTIFIER PRIMARY KEY,
-    eventTypeID INT FOREIGN KEY REFERENCES app.EventType(eventTypeID) NOT NULL,
+    eventTypeID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.EventType(eventTypeID) NOT NULL,
     surveyDescription NVARCHAR(100) NOT NULL,
     isActive BIT NOT NULL,
 );
 CREATE TABLE app.SurveyOption
 (
     surveyOptionID UNIQUEIDENTIFIER PRIMARY KEY,
-    displayText INT NOT NULL,
-    surveyOptionValue NVARCHAR(10) NOT NULL,
+    displayText NVARCHAR(100) NOT NULL,
+    surveyOptionValue NVARCHAR(50) NOT NULL,
 );
 CREATE TABLE app.SurveyQuestion
 (
@@ -67,12 +67,12 @@ CREATE TABLE app.SurveyQuestionXref
     surveyID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.Survey(surveyID) NOT NULL,
     surveyQuestionID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.SurveyQuestion(surveyQuestionID) NOT NULL,
     sortOrder NVARCHAR(5) NOT NULL,
-    isAction BIT NOT NULL
+    isActive BIT NOT NULL
 );
 CREATE TABLE app.SurveyQuestionOptionXref
 (
     surveyQuestionID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.SurveyQuestion(surveyQuestionID) NOT NULL,
     surveyOptionID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES app.SurveyOption(surveyOptionID) NOT NULL,
     sortOrder NVARCHAR(5) NOT NULL,
-    isAction BIT NOT NULL
+    isActive BIT NOT NULL
 );
--- SQL/SQLFillDatabaseTestData.sql
+++ SQL/SQLFillDatabaseTestData.sql
@@ -1,34 +1,94 @@
-INSERT INTO app.SurveyQuestion
-VALUES
-    ();
-INSERT INTO app.SurveyOption
-VALUES
-    ();
-INSERT INTO app.SurveyQuestionXref
-VALUES
-    ();
-INSERT INTO app.EventType
-VALUES
-    ();
-INSERT INTO app.Survey
-VALUES
-    ();
-INSERT INTO app.SurveyQuestionOptionXref
-VALUES
-    ();
-INSERT INTO app.ClientStatus
-VALUES
-    ();
-INSERT INTO app.Client
-VALUES
-    ();
-INSERT INTO app.ClientRelationXref
-VALUES
-    ();
-INSERT INTO app.SurveyResponse
-VALUES
-    ();
+DECLARE @eventTypeIDGUID UNIQUEIDENTIFIER;
+DECLARE @surveyQuestionIDGUID UNIQUEIDENTIFIER;
+DECLARE @surveyIDGUID UNIQUEIDENTIFIER;
+DECLARE @surveyOptionIDGUID UNIQUEIDENTIFIER;
+DECLARE @firstClientStatusIDGUID UNIQUEIDENTIFIER;
+DECLARE @secondClientStatusIDGUID UNIQUEIDENTIFIER;
+DECLARE @senderClientIDGUID UNIQUEIDENTIFIER;
+DECLARE @recipientClientIDGUID UNIQUEIDENTIFIER;
+DECLARE @surveyResponseIDGUID UNIQUEIDENTIFIER;
 
+SET @eventTypeIDGUID = NEWID();
+SET @surveyQuestionIDGUID = NEWID();
+SET @surveyIDGUID = NEWID();
+SET @surveyOptionIDGUID = NEWID();
+SET @surveyResponseIDGUID = NEWID();
+
+SET @firstClientStatusIDGUID = NEWID();
+SET @secondClientStatusIDGUID = NEWID();
+
+SET @senderClientIDGUID = NEWID();
+SET @recipientClientIDGUID = NEWID();
+
+PRINT N'eventTypeIDGUID:------------ ' + (CAST (@eventTypeIDGUID AS NVARCHAR(50)));
+PRINT N'surveyQuestionIDGUID:------- ' + (CAST (@surveyQuestionIDGUID AS NVARCHAR(50)));
+PRINT N'surveyIDGUID:--------------- ' + (CAST (@surveyIDGUID AS NVARCHAR(50)));
+PRINT N'surveyOptionIDGUID:--------- ' + (CAST (@surveyOptionIDGUID AS NVARCHAR(50)));
+PRINT N'surveyResponseIDGUID:------- ' + (CAST (@surveyResponseIDGUID AS NVARCHAR(50)));
+
+PRINT N'firstClientStatusIDGUID:---- ' + (CAST (@firstClientStatusIDGUID AS NVARCHAR(50)));
+PRINT N'secondClientStatusIDGUID:--- ' + (CAST (@secondClientStatusIDGUID AS NVARCHAR(50)));
+
+PRINT N'senderClientIDGUID:--------- ' + (CAST (@senderClientIDGUID AS NVARCHAR(50)));
+PRINT N'recipientClientIDGUID:------ ' + (CAST (@recipientClientIDGUID AS NVARCHAR(50)));
+
+
+
+
+INSERT INTO app.EventType (eventTypeID, eventDescription, isActive)
+VALUES
+    (@eventTypeIDGUID,'Event Description', 1);
+
+INSERT INTO app.SurveyQuestion (surveyQuestionID, questionText, isSurveyOptionList)
+VALUES
+    (@surveyQuestionIDGUID,'Question 1',1);
+
+INSERT INTO app.Survey (surveyID, eventTypeID, surveyDescription, isActive)
+VALUES
+    (@surveyIDGUID, @eventTypeIDGUID, 'surveyDescription', 1);
+
+INSERT INTO app.SurveyOption (surveyOptionID, displayText, surveyOptionValue)
+VALUES
+    (@surveyOptionIDGUID,'Display Text','Survey Option Value');
+
+INSERT INTO app.SurveyQuestionXref (surveyID, surveyQuestionID, sortOrder, isActive)
+VALUES
+    (@surveyIDGUID, @surveyQuestionIDGUID, 'asc', 1);
+
+INSERT INTO app.SurveyQuestionOptionXref (surveyQuestionID, surveyOptionID, sortOrder, isActive)
+VALUES
+    (@surveyQuestionIDGUID, @surveyOptionIDGUID, 'asc', 1);
+    
+INSERT INTO app.ClientStatus (clientStatusID, statusDescription)
+VALUES
+    (@firstClientStatusIDGUID, 'Status Description 1'),
+    (@secondClientStatusIDGUID, 'Status Description 2');
+    
+INSERT INTO app.Client (clientID, clientStatusID, clientName, nickname, email, addressLine1, addressLine2, city, [state], postalCode, country)
+VALUES
+    (@senderClientIDGUID, @firstClientStatusIDGUID, 'Sender Client Name', 'First Nickname', 'firstemail@email.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country'),
+    (@recipientClientIDGUID, @secondClientStatusIDGUID, 'Recipient Client Name', 'Second Nickname', 'secondemail@email.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country');
+
+INSERT INTO app.SurveyResponse (surveyResponseID, surveyID, clientID, surveyQuestionID, surveyOptionID, responseText)
+VALUES
+    (@surveyResponseIDGUID, @surveyIDGUID, @senderClientIDGUID, @surveyQuestionIDGUID, @surveyOptionIDGUID, 'Response Text');
+    
+INSERT INTO app.ClientRelationXref (senderClientID, recipientClientID, eventTypeID)
+VALUES
+    (@senderClientIDGUID, @recipientClientIDGUID, @eventTypeIDGUID);
+
+
+
+SELECT * FROM app.EventType;
+SELECT * FROM app.SurveyQuestion;
+SELECT * FROM app.Survey;
+SELECT * FROM app.SurveyOption;
+SELECT * FROM app.SurveyQuestionXref;
+SELECT * FROM app.SurveyQuestionOptionXref;
+SELECT * FROM app.ClientStatus;
+SELECT * FROM app.Client;
+SELECT * FROM app.SurveyResponse;
+SELECT * FROM app.ClientRelationXref;
 
 
 
