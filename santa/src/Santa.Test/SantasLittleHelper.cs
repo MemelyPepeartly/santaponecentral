27fca2a473191c3671c307f93f958da29998f41a
TheMemelyMan <memelypepeartly@gmail.com>
2020-02-20 01:59:54 -0500

    more helper setup

--- santa/src/Santa.Test/SantasLittleHelper.cs
+++ santa/src/Santa.Test/SantasLittleHelper.cs
@@ -23,26 +23,87 @@ namespace Santa.Test
         public List<Survey> Surveys { get; private set; }
         public List<Question> Questions { get; private set; }
         public List<Option> QuestionOptions { get; private set; }
+        public Address TestAddress { get; private set; }
 
         public SantasLittleHelper()
         {
-
+            SetUpClients();
+            SetUpQuestions();
+            SetUpQuestionOptions();
+            SetUpSurveys();
+            
+            TestAddress = new Address
+            {
+                addressLineOne = "123 Test Street",
+                addressLineTwo = "Apt 123",
+                city = "TestCity",
+                state = "TestState",
+                postalCode = "12345",
+                country = "USA"
+            };
         }
         private void SetUpClients()
+        {
+            Clients = new List<Client>
+            {
+                new Client
+                {
+                    clientName = "Test Name 1",
+                    email = "test1@test.com",
+                    nickname = "Test Nickname 1",
+                    address = TestAddress
+                },
+                new Client
+                {
+                    clientName = "Test Name 2",
+                    email = "test2@test.com",
+                    nickname = "Test Nickname 2",
+                    address = TestAddress
+                },
+                new Client
+                {
+                    clientName = "Test Name 3",
+                    email = "test3@test.com",
+                    nickname = "Test Nickname 3",
+                    address = TestAddress
+                }
+            };
+        }
+        private void SetUpQuestions()
+        {
+            Questions = new List<Question>
+            {
+                new Question
+                {
+
+                },
+                new Question
+                {
+
+                }
+            };
+        }
+        private void SetUpQuestionOptions()
         {
 
         }
         private void SetUpSurveys()
         {
-
-        }
-        private void SetUpQuestions()
-        {
-
-        }
-        private void SetUpQuestionOptions()
-        {
-
+            Surveys = new List<Survey>
+            {
+                new Survey
+                {
+                    surveyDescription = "Survey Description 1",
+                    active = true,
+                    surveyQuestions = Questions
+                },
+                new Survey
+                {
+                    surveyDescription = "Survey Description 2",
+                    active = false,
+                    surveyQuestions = Questions
+                }
+            };
         }
     }
 }
