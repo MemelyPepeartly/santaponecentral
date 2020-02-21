0187cb634f4d3d186eeb8d25e8c4893e2b87fcc7
TheMemelyMan <memelypeperartly@gmail.com>
2020-02-20 19:24:40 -0500

    Generated Xunit tests

---
+++ santa/src/Santa.Test/Controllers/EventControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using Santa.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Api.Controllers.Tests
+{
+    public class EventControllerTests
+    {
+        [Fact()]
+        public void EventControllerTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void GetTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void GetTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void PostTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void PutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void DeleteTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ santa/src/Santa.TestTests/ApiTest/SurveyControllerTestsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Santa.Test.ApiTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Test.ApiTest.Tests
+{
+    [TestClass()]
+    public class SurveyControllerTestsTests
+    {
+        [TestMethod()]
+        public void Test1Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
