1f0b5d7b2d59db7d0d32d8b1c139b371e0257ff1
TheMemelyMan <memelypeperartly@gmail.com>
2020-03-11 03:24:41 -0400

    New controllers for atomicity sake

--- santa/src/Santa.Logic/Objects/Question.cs
+++ santa/src/Santa.Logic/Objects/Question.cs
@@ -7,15 +7,7 @@ namespace Santa.Logic.Objects
     /// </summary>
     public class Question
     {
-        public Question(Guid _surveyID)
-        {
-            surveyID = _surveyID;
-        }
-
         public Guid questionID { get; set; }
-        public Guid surveyID { get; set; }
-        public string sortOrder { get; set; }
-        public bool isActive { get; set; }
         public string questionText { get; set; }
         public bool isSurveyOptionList { get; set; }
         public List<Option> surveyOptionList { get; set; }
