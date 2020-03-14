7a8b0cba50999bde1d5afe5cc0a2ece1b0fc17d9
TheMemelyMan <memelypeperartly@gmail.com>
2020-03-14 19:36:17 -0400

    Added angular service for API calls

--- santa/src/Santa.Api/Startup.cs
+++ santa/src/Santa.Api/Startup.cs
@@ -43,7 +43,7 @@ namespace Santa.Api
                 options.AddPolicy("AllowAngular",
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:5001", "http://dev-santaponecentral.azurewebsites.net")
+                    builder.WithOrigins("http://localhost:4200", "http://dev-santaponecentral.azurewebsites.net")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials();
