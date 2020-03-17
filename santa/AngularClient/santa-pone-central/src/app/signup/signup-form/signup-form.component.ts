a804e82bbc5de8f8992d567f09dda9dfda7ae5a9
TheMemelyMan <memelypepeartly@gmail.com>
2020-03-17 03:26:27 -0400

    Getting this dumbass shit to work

--- santa/AngularClient/santa-pone-central/src/app/signup/signup-form/signup-form.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/signup/signup-form/signup-form.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Client, ClientResponse } from 'src/classes/Client';
 import { SantaApiGetService, SantaApiPostService } from 'src/app/services/SantaApiService.service';
 import { EventType } from 'src/classes/EventType';
 import { Status } from 'src/classes/Status';
 import { MapService } from '../../services/MapService.service';
-import { EventConstants } from 'src/shared/constants/EventConstants';
+import { EventConstants } from '../../shared/constants/EventConstants';
 import { Guid } from "guid-typescript";
 
 
@@ -16,30 +16,54 @@ import { Guid } from "guid-typescript";
 })
 export class SignupFormComponent implements OnInit {
 
-  constructor(public SantaGet: SantaApiGetService, public SantaPost: SantaApiPostService, public mapper: MapService) { }
+  constructor(public SantaGet: SantaApiGetService,
+    public SantaPost: SantaApiPostService,
+    public mapper: MapService,
+    private formBuilder: FormBuilder) { }
 
   private client: Client = new Client();
   private events: Array<EventType> = [];
   private statuses: Array<Status> = [];
   private constants: EventConstants = new EventConstants();
 
+  public firstFormGroup: FormGroup;
+  public secondFormGroup: FormGroup;
+
   ngOnInit() {
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    
+    this.secondFormGroup = this.formBuilder.group({
+      addressLineOne: ['', Validators.required],
+      addressLineTwo: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      postalCode: ['', Validators.required],
+    });
+
     //API Call for getting statuses
     this.SantaGet.getAllStatuses().subscribe(res => {
       res.forEach(status => {
         this.statuses.push(this.mapper.mapStatus(status))
       });
     });
-
+    console.log("Statuses");
+    console.log(this.statuses);
     //API Call for getting events
     this.SantaGet.getAllEvents().subscribe(res => {
       res.forEach(eventType => {
         this.events.push(this.mapper.mapEvent(eventType))
       });
     });
+    console.log("Events");
+    console.log(this.events);
   }
   public onSubmit(clientForm: NgForm)
   {
+    console.log(this.firstFormGroup);
+    console.log(this.secondFormGroup);
+    console.log(clientForm);
     let newClient: ClientResponse = new ClientResponse();
     newClient.clientName = clientForm.value.firstName + " " + clientForm.value.lastName;
     newClient.email = clientForm.value.email;
@@ -53,6 +77,7 @@ export class SignupFormComponent implements OnInit {
     newClient.clientStatusID = this.statuses.find(status => status.statusDescription == this.constants.AWAITING).statusID;
     
     this.SantaPost.postClient(Guid.create().toString(), newClient).subscribe(createRes => {
+      console.log(createRes);
       clientForm.resetForm();
     });
   }
