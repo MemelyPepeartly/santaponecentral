89138170383b01caa9ebf3e6a2648af2156ae6bc
TheMemelyMan <memelypeperartly@gmail.com>
2020-03-18 20:23:01 -0400

    animation fade in/out

--- santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/selected-anon.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/selected-anon.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Client } from '../../../classes/client';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-selected-anon',
   templateUrl: './selected-anon.component.html',
-  styleUrls: ['./selected-anon.component.css']
+  styleUrls: ['./selected-anon.component.css'],
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('simpleFadeAnimation', [
+
+      // the "in" style determines the "resting" state of the element when it is visible.
+      state('in', style({opacity: 1})),
+
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(600 )
+      ]),
+
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate(600, style({opacity: 0})))
+    ])
+  ]
 })
 export class SelectedAnonComponent implements OnInit {
 
