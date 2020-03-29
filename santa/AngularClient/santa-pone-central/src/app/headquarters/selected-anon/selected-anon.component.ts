ffe1791abc3afabe3501875f2a7b855739eca696
TheMemelyMan <memelypepeartly@gmail.com>
2020-03-29 06:19:18 -0400

    removed logging

--- santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/selected-anon.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/selected-anon.component.ts
@@ -146,13 +146,6 @@ export class SelectedAnonComponent implements OnInit {
     let relationshipResponse: ClientRelationshipResponse = new ClientRelationshipResponse;
     var currentEvent = this.selectedRecipientEvent;
     var currentSelectedClientID = this.client.clientID;
-    console.log("---------------START OF PROCESS---------------");
-    console.log("Recipients: ");
-    console.log("    -" + this.recipients.length);
-    console.log("Senders: ");
-    console.log("    -" + this.senders.length);
-
-    console.log("Step 1: Post requests");
 
     for (let i = 0; i < this.selectedRecipients.length; i++) {
       console.log(this.selectedRecipients[i]);
@@ -160,10 +153,8 @@ export class SelectedAnonComponent implements OnInit {
       relationshipResponse.eventTypeID = this.selectedRecipientEvent.eventTypeID;
       relationshipResponse.recieverClientID = this.selectedRecipients[i].clientID
 
-      console.log("    Posting " + this.selectedRecipients[i].clientNickname);
       this.client = this.ApiMapper.mapClient(await this.SantaApiPost.postClientRelation(currentSelectedClientID, relationshipResponse).toPromise().catch(err => console.log(err)));
-
-      console.log("    Gathering recipients and senders");
+      
       await this.gatherRecipients();
       await this.gatherSenders();
 
@@ -171,39 +162,24 @@ export class SelectedAnonComponent implements OnInit {
       this.action.emit(this.actionTaken);
       this.addRecipientSuccess = true;
     }
-    
 
-  console.log("Step 2: getAllowedRecipientsByEvent started");
-  await this.getAllowedRecipientsByEvent(currentEvent);
-
-  
-  console.log("---------------END OF PROCESS---------------");
-  console.log("Recipients: ");
-  console.log("    -" + this.recipients.length);
-  console.log("Senders: ");
-  console.log("    -" + this.senders.length);
-   this.showRecipientListPostingSpinner = false; 
+    await this.getAllowedRecipientsByEvent(currentEvent);
+    this.showRecipientListPostingSpinner = false; 
   }
+
   async getAllowedRecipientsByEvent(eventType: EventType)
   {
     this.recipientsAreLoaded = false;
     this.selectedRecipientEvent = eventType;
-    console.log("Step 3: Clear approved recipients");
     this.approvedRecipientClients = [];
     var recipientIDList = [];
-
-    //Foreach relationship in recipients, if the relationshipID is equal to the eventTypeID, add it to an ID list of recipients already in the client profile.
-    console.log("Step 4: Put recipient ID's into a list");
     
     for (let i = 0; i < this.recipients.length; i++) {
       if(this.recipients[i].clientEventTypeID == eventType.eventTypeID)
       {
-        console.log("    Relationship added: " + this.recipients[i].clientNickname);
         recipientIDList.push(this.recipients[i].clientID);
       }
     }
-
-    console.log("Step 5: Grab all the members in DB");
     //Grab all the members in the DB
     var clientsInDBRes = await this.SantaApiGet.getAllClients().toPromise();
 
@@ -213,16 +189,12 @@ export class SelectedAnonComponent implements OnInit {
     //the client from DB is not in the list of the selected client's recipient ID list by event already
     //Push a new possible relationship into the approvedRecipientClient's list
 
-    console.log("Step 6: Foreach client in the response, get the approved ones for the list");
-
     for(let i = 0; i< clientsInDBRes.length; i++)
     {
       var mappedClient: Client = this.ApiMapper.mapClient(clientsInDBRes[i]);
 
       if(mappedClient.clientStatus.statusDescription == EventConstants.APPROVED && mappedClient.clientID != this.client.clientID && recipientIDList.includes(mappedClient.clientID) == false)
       {
-        console.log("    Adding " + mappedClient.clientNickname + " to approvedRecipientClients");
-        
         this.approvedRecipientClients.push(this.ApiMapper.mapClientRelationship(clientsInDBRes[i], eventType.eventTypeID));
       }
     }
@@ -236,7 +208,6 @@ export class SelectedAnonComponent implements OnInit {
     {
       this.recipients.push(this.ApiMapper.mapClientRelationship(await this.SantaApiGet.getClient(this.client.recipients[i].recipientClientID).toPromise(), this.client.recipients[i].recipientEventTypeID));
     }
-    console.log("        Finished gathering recipients");
   }
   async gatherSenders()
   {
@@ -247,7 +218,6 @@ export class SelectedAnonComponent implements OnInit {
     {
       this.senders.push(this.ApiMapper.mapClientRelationship(await this.SantaApiGet.getClient(this.client.senders[i].senderClientID).toPromise(), this.client.senders[i].senderClientID));
     }
-    console.log("        Finished gathering senders");
   }
   gatherEvents()
   {
