13631b324f5a0910d1b1d0768862d973e5685927
TheMemelyMan <memelypepeartly@gmail.com>
2020-04-17 09:52:53 -0400

    Changed to country selection with service

---
+++ santa/AngularClient/santa-pone-central/src/app/services/Countries.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { CountriesService } from './Countries.service';
+
+describe('Service: Countries', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CountriesService]
+    });
+  });
+
+  it('should ...', inject([CountriesService], (service: CountriesService) => {
+    expect(service).toBeTruthy();
+  }));
+});
