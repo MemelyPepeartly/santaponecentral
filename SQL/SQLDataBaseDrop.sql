ff32b5cf9b96eae9c7f011a83c2e4f1b72ac7187
TheMemelyMan <memelypepeartly@gmail.com>
2020-04-23 18:34:53 -0400

    Added tags to SQL commands

--- SQL/SQLDataBaseDrop.sql
+++ SQL/SQLDataBaseDrop.sql
@@ -1,15 +1,14 @@
 DROP TABLE app.SurveyResponse;
 DROP TABLE app.SurveyQuestionOptionXref;
 DROP TABLE app.SurveyOption;
-
 DROP TABLE app.SurveyQuestionXref;
 DROP TABLE app.SurveyQuestion;
 DROP TABLE app.Survey;
-
 DROP TABLE app.ClientRelationXref;
+DROP TABLE app.EventType;
+DROP TABLE app.ClientTagXref;
 DROP TABLE app.Client;
 DROP TABLE app.ClientStatus;
-
-DROP TABLE app.EventType;
+DROP TABLE app.Tag;
 
 DROP SCHEMA app;
