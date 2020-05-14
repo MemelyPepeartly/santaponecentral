6faf8adbbe78973cc8f87eafc0067a909edcd54f
TheMemelyMan <memelypepeartly@gmail.com>
2020-05-13 20:33:34 -0400

    setup

--- santa/AngularClient/santa-pone-central/src/app/services/Gatherer.service.ts
+++ santa/AngularClient/santa-pone-central/src/app/services/Gatherer.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { SantaApiGetService } from './SantaApiService.service';
+import { MapService } from './MapService.service';
+import { Client } from 'src/classes/client';
+import { Tag } from 'src/classes/tag';
+import { Survey, Question } from 'src/classes/survey';
+import { EventType } from 'src/classes/EventType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GathererService {
 
-  constructor() { }
+  constructor(public SantaApiGet: SantaApiGetService, public ApiMapper: MapService) { }
+
+  public allClients: Array<Client> = []
+  public allTags: Array<Tag> = []
+  public allSurveys: Array<Survey> = []
+  public allQuestions: Array<Question> = []
+  public allEvents: Array<EventType> = []
+
+  
+  public async gatherAllClients()
+  {
+
+  }
+  public async gatherAllTags()
+  {
+
+  }
+  public async gatherAllSurveys()
+  {
+
+  }
+  public async gatherAllQuestions()
+  {
+
+  }
+  public async gatherAllEvents()
+  {
+
+  }
 }
