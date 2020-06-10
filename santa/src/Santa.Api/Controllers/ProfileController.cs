44045cfc9121bd5a86c3a04e4c4b95c375775938
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-09 22:37:01 -0400

    New profile controller with get

---
+++ santa/src/Santa.Api/Controllers/ProfileController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Santa.Logic.Interfaces;
+
+namespace Santa.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IRepository repository;
+        public ProfileController(IRepository _repository)
+        {
+            repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
+
+        }
+
+        // GET: api/Client/5
+        /// <summary>
+        /// Gets a client's profile by their email
+        /// 
+        /// Conditions: Have an Auth0 account, implying you have been approved
+        /// </summary>
+        /// <param name="clientID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<Logic.Objects.Client>> GetProfileByEmailAsync()
+        {
+            try
+            {
+                Logic.Objects.Client client = await repository.GetClientByEmailAsync("");
+                if(client == null)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return Ok(client);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+
+        }
+    }
+}
