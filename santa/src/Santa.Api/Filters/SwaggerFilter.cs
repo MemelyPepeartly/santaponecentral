ea8b3a595e80774aac1b351051fb22c5f50b78b1
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-09 22:26:51 -0400

    Swagger filter for fun stuff later

---
+++ santa/src/Santa.Api/Filters/SwaggerFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Dbnd.Api.Filters
+{
+    // allows Swagger to understand that [Authorize] attribute means
+    // that Bearer auth is required, or else 401 response.
+    public class SwaggerFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var methodAttributes = context.MethodInfo.GetCustomAttributes(true);
+            var controllerAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true);
+
+            var methodAllowAnonymous = methodAttributes.OfType<AllowAnonymousAttribute>().Any();
+            var authorize = methodAttributes.Union(controllerAttributes).OfType<AuthorizeAttribute>().Any();
+
+            if (!methodAllowAnonymous && authorize)
+            {
+                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+
+                var bearerScheme = new OpenApiSecurityScheme
+                {
+                    // the id here must match the security scheme name defined in the startup file
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "BearerAuth" }
+                };
+
+                operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    // scope list should be empty when definition type is bearer
+                    new OpenApiSecurityRequirement { [ bearerScheme ] = Array.Empty<string>() }
+                };
+            }
+        }
+    }
+}
