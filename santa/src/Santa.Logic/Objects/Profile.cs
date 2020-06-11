1add8f9ceb44455b986b229574575645f78ce842
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-10 20:08:21 -0400

    New Profile logic object

---
+++ santa/src/Santa.Logic/Objects/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class Profile
+    {
+        public Guid clientID { get; set; }
+        public Status clientStatus { get; set; }
+        public string clientName { get; set; }
+        public string nickname { get; set; }
+        public string email { get; set; }
+        public Address address { get; set; }
+        public List<Recipient> recipients { get; set; }
+        public List<Response> responses { get; set; }
+    }
+}
