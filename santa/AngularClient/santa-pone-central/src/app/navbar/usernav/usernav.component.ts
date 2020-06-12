a67b75e5544c37117b4c308cfc3d45c071ffc900
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-12 12:03:30 -0400

    auth service constructor

--- santa/AngularClient/santa-pone-central/src/app/navbar/usernav/usernav.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/navbar/usernav/usernav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-usernav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsernavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
