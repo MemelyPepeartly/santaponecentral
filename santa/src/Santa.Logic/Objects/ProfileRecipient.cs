066a2145d6e74133689711693980bf1e72fc9375
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-12 11:53:15 -0400

    New logic profile recipient type

---
+++ santa/src/Santa.Logic/Objects/ProfileRecipient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class ProfileRecipient
+    {
+        public Guid recipientClientID { get; set; }
+        public Guid recipientEventTypeID { get; set; }
+        public string name { get; set; }
+        public string nickname { get; set; }
+        public Address address { get; set; }
+        public List<Response> responses { get; set; }
+
+    }
+}
