68bb23aefe529b75f5d376e03bfb962db17cd0c7
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-15 16:52:23 -0400

    controller funcitonality implimented

--- santa/src/Santa.Api/Controllers/MessageController.cs
+++ santa/src/Santa.Api/Controllers/MessageController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Santa.Api.Models.Message_Models;
 using Santa.Logic.Interfaces;
 
 namespace Santa.Api.Controllers
@@ -24,7 +26,7 @@ namespace Santa.Api.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult<List<Logic.Objects.Message>> Get()
+        public ActionResult<List<Logic.Objects.Message>> GetAllMessages()
         {
             try
             {
@@ -42,24 +44,49 @@ namespace Santa.Api.Controllers
         /// </summary>
         /// <param name="messageID"></param>
         /// <returns></returns>
-        [HttpGet("{messageID}", Name = "Get")]
-        public async Task<ActionResult<Logic.Objects.Message>> Get(Guid messageID)
+        [HttpGet("{chatMessageID}", Name = "Get")]
+        public async Task<ActionResult<Logic.Objects.Message>> GetMessage(Guid chatMessageID)
         {
             try
             {
-                return Ok(await repository.GetMessageByIDAsync(messageID));
+                return Ok(await repository.GetMessageByIDAsync(chatMessageID));
             }
             catch(Exception e)
             {
                 throw e.InnerException;
             }
-            
         }
 
         // POST: api/Message
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Logic.Objects.Message>> PostMessage([FromBody, Bind("messageSenderClientID, messageRecieverClientID, messageContent, clientRelationXrefID")] ApiMessage message)
         {
+            try
+            {
+                Logic.Objects.Message logicMessage = new Logic.Objects.Message()
+                {
+                    chatMessageID = Guid.NewGuid(),
+                    messageRecieverClientID = message.messageRecieverClientID == null ? null : message.messageRecieverClientID,
+                    messageSenderClientID = message.messageSenderClientID == null ? null : message.messageSenderClientID,
+                    clientRelationXrefID = message.clientRelationXrefID == null ? null : message.clientRelationXrefID,
+                    messageContent = message.messageContent,
+                    isMessageRead = false,
+                };
+                if (logicMessage.messageRecieverClientID == null && logicMessage.messageSenderClientID == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+                else
+                {
+                    await repository.CreateMessage(logicMessage);
+                    await repository.SaveAsync();
+                    return Ok(await repository.GetMessageByIDAsync(logicMessage.chatMessageID));
+                }
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
         }
     }
 }
