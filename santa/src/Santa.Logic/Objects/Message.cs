82ec5f4e440a5b721802bf5c5e1bff0075c5454f
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-15 04:05:49 -0400

    Message type

---
+++ santa/src/Santa.Logic/Objects/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class Message
+    {
+        public Guid chatMessageID { get; set; }
+        public Guid? messageSenderClientID { get; set; }
+        public Guid? messageRecieverClientID { get; set; }
+        public Guid? clientRelationXrefID { get; set; }
+        public bool isMessageRead { get; set; }
+        public string messageContent { get; set; }
+    }
+}
