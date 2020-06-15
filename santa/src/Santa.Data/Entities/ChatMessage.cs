05d05acebed5c7d1d973efe808ba9de1e96da99b
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-15 03:56:56 -0400

    Rescaffold with DB changes

---
+++ santa/src/Santa.Data/Entities/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Santa.Data.Entities
+{
+    public partial class ChatMessage
+    {
+        public Guid ChatMessageId { get; set; }
+        public Guid? MessageSenderClientId { get; set; }
+        public Guid? MessageRecieverClientId { get; set; }
+        public Guid? ClientRelationXrefId { get; set; }
+        public bool IsMessageRead { get; set; }
+        public string MessageContent { get; set; }
+
+        public virtual ClientRelationXref ClientRelationXref { get; set; }
+        public virtual Client MessageRecieverClient { get; set; }
+        public virtual Client MessageSenderClient { get; set; }
+    }
+}
--- santa/src/Santa.Data/Entities/ClientRelationXref.cs
+++ santa/src/Santa.Data/Entities/ClientRelationXref.cs
@@ -5,7 +5,12 @@ namespace Santa.Data.Entities
 {
     public partial class ClientRelationXref
     {
-        public int ClientRelationXrefId { get; set; }
+        public ClientRelationXref()
+        {
+            ChatMessage = new HashSet<ChatMessage>();
+        }
+
+        public Guid ClientRelationXrefId { get; set; }
         public Guid SenderClientId { get; set; }
         public Guid RecipientClientId { get; set; }
         public Guid EventTypeId { get; set; }
@@ -13,5 +18,6 @@ namespace Santa.Data.Entities
         public virtual EventType EventType { get; set; }
         public virtual Client RecipientClient { get; set; }
         public virtual Client SenderClient { get; set; }
+        public virtual ICollection<ChatMessage> ChatMessage { get; set; }
     }
 }
