e9e4db7b8b7bda2ba7c2b48bd664e9c944309961
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-15 03:39:07 -0400

    Message controller generation

---
+++ santa/src/Santa.Api/Controllers/MessageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Santa.Logic.Interfaces;
+
+namespace Santa.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IRepository repository;
+        public MessageController(IRepository _repository)
+        {
+            repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
+
+        }
+        // GET: api/Message
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Message/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Message
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Message/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
