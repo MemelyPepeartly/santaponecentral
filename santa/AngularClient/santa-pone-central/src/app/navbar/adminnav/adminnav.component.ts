4fb2b1332005e0b8c6fc115b012d210dc10bf66a
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-15 21:37:44 -0400

    nav to handle admin vs participant

--- santa/AngularClient/santa-pone-central/src/app/navbar/adminnav/adminnav.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/navbar/adminnav/adminnav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-adminnav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminnavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
