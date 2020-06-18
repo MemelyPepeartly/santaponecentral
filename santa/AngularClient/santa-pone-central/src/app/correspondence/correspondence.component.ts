dd5b5970819911e11737d40dd9282787aa63c8f1
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-18 01:16:31 -0400

    correspondence setup

---
+++ santa/AngularClient/santa-pone-central/src/app/correspondence/correspondence.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-correspondence',
+  templateUrl: './correspondence.component.html',
+  styleUrls: ['./correspondence.component.css']
+})
+export class CorrespondenceComponent implements OnInit {
+
+  constructor() { }
+
+  displayedColumns: string[] = ['sender', 'recipient', 'event'];
+
+  ngOnInit() {
+  }
+
+}
