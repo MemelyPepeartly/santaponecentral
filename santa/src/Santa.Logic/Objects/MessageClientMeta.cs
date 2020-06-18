f9ea167a3c8b73ee644e6c95caadfb698bcd8008
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-18 03:46:24 -0400

    new info for messages. Datime and meta info added

---
+++ santa/src/Santa.Logic/Objects/MessageClientMeta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class MessageClientMeta
+    {
+        public Guid? clientId { get; set; }
+        public string clientName { get; set; }
+        public string clientNickname { get; set; }
+    }
+}
