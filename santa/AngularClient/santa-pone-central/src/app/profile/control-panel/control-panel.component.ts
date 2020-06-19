7f13084b28fc8bce11d9e17088f00fd446ac6e3f
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-19 03:07:48 -0400

    Profile control panel refined and updated

--- santa/AngularClient/santa-pone-central/src/app/profile/control-panel/control-panel.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/control-panel/control-panel.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Client } from 'src/classes/client';
 import { ProfileRecipient } from 'src/classes/profile';
+import { GathererService } from 'src/app/services/gatherer.service';
+import { EventType } from 'src/classes/eventType';
 
 @Component({
   selector: 'app-control-panel',
@@ -9,9 +11,10 @@ import { ProfileRecipient } from 'src/classes/profile';
 })
 export class ControlPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gatherer: GathererService) { }
 
   @Input() recipients: Array<ProfileRecipient> = []
+  columns: string[] = ["recipient", "event", "contact"];
 
   ngOnInit(): void {
   }
