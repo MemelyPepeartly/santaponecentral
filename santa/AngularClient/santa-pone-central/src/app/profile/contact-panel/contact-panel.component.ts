345e1f88e510fe39c024d5bc74e0b7f897553867
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-21 07:32:17 -0400

    profile funcitonality start

--- santa/AngularClient/santa-pone-central/src/app/profile/contact-panel/contact-panel.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/contact-panel/contact-panel.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Message } from 'src/classes/message';
+import { MapService, MapResponse } from 'src/app/services/mapService.service';
+import { SantaApiPostService } from 'src/app/services/santaApiService.service';
+import { ProfileRecipient, Profile } from 'src/classes/profile';
 
 @Component({
   selector: 'app-contact-panel',
@@ -8,11 +11,22 @@ import { Message } from 'src/classes/message';
 })
 export class ContactPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(public SantaApiPost: SantaApiPostService, public responseMapper: MapResponse) { }
 
   @Input() chat: Array<Message>;
+  @Input() selectedChatHistory: ProfileRecipient;
+  @Input() profile: Profile;
+
+  @Output() messagePosted: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   ngOnInit(): void {
+  }
+
+  public async postTest()
+  {
+    var newMessageResponse = this.responseMapper.mapMessageResponse(this.selectedChatHistory, this.profile.clientID, "TEST POST")
+    var res = await this.SantaApiPost.postMessage(newMessageResponse).toPromise();
+    this.messagePosted.emit(true);   
   }
 
 }
