f885c50de3e4e9e10ba5f346499ea4d988c86a89
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-24 08:28:26 -0400

    Big api update with unread counts for front end with message histories

--- santa/src/Santa.Logic/Objects/Profile.cs
+++ santa/src/Santa.Logic/Objects/Profile.cs
@@ -12,6 +12,7 @@ namespace Santa.Logic.Objects
         public string nickname { get; set; }
         public string email { get; set; }
         public Address address { get; set; }
+        public int generalChatUnreadCount { get; set; }
         public List<ProfileRecipient> recipients { get; set; }
         public List<Response> responses { get; set; }
     }
