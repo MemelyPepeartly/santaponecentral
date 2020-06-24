76461eb4991a0a3740de39bd57512c71394237c9
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-24 08:27:55 -0400

    better implimentation with history model

---
+++ santa/src/Santa.Logic/Objects/MessageHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class MessageHistory
+    {
+        public List<Message> history { get; set; }
+
+    }
+}
