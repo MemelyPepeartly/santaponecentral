13f601ab19e0a0ea99b13fdca6d5e0d92d4ff049
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-25 06:08:06 -0400

    removed unused endpoint

--- santa/src/Santa.Api/Controllers/SurveyController.cs
+++ santa/src/Santa.Api/Controllers/SurveyController.cs
@@ -137,26 +137,6 @@ namespace Santa.Api.Controllers
             }
         }
 
-        // POST: api/Survey/5/SurveyQuestions/5
-        /// <summary>
-        /// Posts a new relation between a survey and an existing question by surveyID and surveyQuestionID
-        /// </summary>
-        /// <param name="surveyID"></param>
-        /// <param name="surveyQuestionID"></param>
-        /// <returns></returns>
-        [HttpPost("{surveyID}/Relationship")]
-        public async Task<ActionResult<ApiSurvey>> PostSurveyQuestionRelation(Guid surveyID, Guid surveyQuestionID)
-        {
-            try
-            {
-                throw new NotImplementedException();
-            }
-            catch(Exception e)
-            {
-                throw e.InnerException;
-            }
-        }
-
         // PUT: api/Survey/5
         /// <summary>
         /// Put updates a survey's description by survey ID. Binds to ApiSurveyDescription model.
