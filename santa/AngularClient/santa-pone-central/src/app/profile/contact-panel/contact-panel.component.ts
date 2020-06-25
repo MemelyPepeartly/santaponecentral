bbb4ba6e91c41f50237dfe3275f600eb11a07954
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-25 06:13:04 -0400

    heavy contact panel refactoring with new service

--- santa/AngularClient/santa-pone-central/src/app/profile/contact-panel/contact-panel.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/contact-panel/contact-panel.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { Message } from 'src/classes/message';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Message, MessageHistory } from 'src/classes/message';
 import { MapService, MapResponse } from 'src/app/services/mapService.service';
 import { SantaApiPostService } from 'src/app/services/santaApiService.service';
 import { ProfileRecipient, Profile } from 'src/classes/profile';
 import { AuthService } from 'src/app/auth/auth.service';
+import { ProfileService } from 'src/app/services/Profile.service';
 
 @Component({
   selector: 'app-contact-panel',
@@ -12,32 +13,47 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class ContactPanelComponent implements OnInit {
 
-  constructor(public SantaApiPost: SantaApiPostService, public responseMapper: MapResponse, public auth: AuthService) { }
+  constructor(public SantaApiPost: SantaApiPostService, public responseMapper: MapResponse, public auth: AuthService, public profileService: ProfileService) { }
 
-  @Input() chat: Array<Message>;
-  @Input() selectedChatHistory: ProfileRecipient;
+  @Input() selectedHistory: MessageHistory;
+  @Input() selectedProfileRecipient: ProfileRecipient;
   @Input() profile: Profile;
-  public isAdmin: boolean;
 
   @Output() messagePosted: EventEmitter<boolean> = new EventEmitter<boolean>();
+  
+  @ViewChild('chatFrame', {static: false}) chatFrame: ElementRef;
+
+  public isAdmin: boolean;
+
 
   ngOnInit(): void {
     this.auth.isAdmin.subscribe((admin: boolean) => {
       this.isAdmin = admin;
     });
   }
+  ngAfterViewChecked() {        
+    this.scrollToBottom();      
+  } 
 
   public async memberPostTest()
   {
-    var newMessageResponse = this.responseMapper.mapMessageResponse(this.selectedChatHistory, "I am a member posting", this.profile.clientID, null)
+    let test = new ProfileRecipient();
+    var newMessageResponse = this.responseMapper.mapMessageResponse(test, "I am a member posting", this.profile.clientID, null)
     var res = await this.SantaApiPost.postMessage(newMessageResponse).toPromise();
-    this.messagePosted.emit(true);   
+    this.messagePosted.emit(true);
   }
   public async adminPostTest()
   {
-    var newMessageResponse = this.responseMapper.mapMessageResponse(this.selectedChatHistory, "I am an admin posting", null, this.profile.clientID)
+    var newMessageResponse = this.responseMapper.mapMessageResponse(this.selectedProfileRecipient, "I am an admin posting", null, this.profile.clientID)
     var res = await this.SantaApiPost.postMessage(newMessageResponse).toPromise();
-    this.messagePosted.emit(true);   
+    this.messagePosted.emit(true); 
   }
-
+  public async scrollToBottom()
+  {
+    try 
+    {
+      this.chatFrame.nativeElement.scrollTop = this.chatFrame.nativeElement.scrollHeight;
+    }
+    catch(err) { }
+  }
 }
