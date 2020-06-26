f54d9547b696272de39e1278aa29970e1b9ffa78
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-26 14:58:42 -0400

    chat histories component updated for functionality

--- santa/AngularClient/santa-pone-central/src/app/shared/chat-histories/chat-histories.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/shared/chat-histories/chat-histories.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { MessageHistory } from 'src/classes/message';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MessageHistory, MessageMeta } from 'src/classes/message';
+import { ProfileService } from 'src/app/services/Profile.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { EventType } from 'src/classes/eventType';
 
 @Component({
   selector: 'app-chat-histories',
@@ -8,13 +11,38 @@ import { MessageHistory } from 'src/classes/message';
 })
 export class ChatHistoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private profileService: ProfileService, private auth: AuthService) { }
 
-  columns: string[] = ["recipient", "event", "contact"];
+  @Input() onProfile: boolean;
+
+  columns: string[] = ["sender", "recipient", "event", "contact"];
+
+  public isAdmin: boolean = false;
 
   @Input() histories: Array<MessageHistory>
 
+  @Output() chatSelectedEvent: EventEmitter<MessageHistory> = new EventEmitter<MessageHistory>();
+  @Output() recipientSelectedEvent: EventEmitter<{meta: MessageMeta, event: EventType}> = new EventEmitter<{meta: MessageMeta, event: EventType}>();
+
+
   ngOnInit(): void {
+    this.auth.isAdmin.subscribe((admin: boolean) => {
+      this.isAdmin = admin;
+    });
   }
+  public emitSelectedHistory(history: MessageHistory)
+  {
+    this.chatSelectedEvent.emit(history);
+  }
+  public emitSelectedRecipientInformation(historyMeta: MessageMeta, historyEvent: EventType)
+  {
+    this.recipientSelectedEvent.emit({meta: historyMeta, event: historyEvent});
+  }
+  public log()
+  {
+    console.log(this.isAdmin);
+    console.log(this.onProfile);
+  }
+
 
 }
