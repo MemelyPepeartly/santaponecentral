2667efd5d9977126c22cfeeda321c6cb1b78c39d
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-27 06:38:49 -0400

    major component funcitonality implimentation

--- santa/AngularClient/santa-pone-central/src/app/correspondence/correspondence.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/correspondence/correspondence.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageHistory, ClientMeta } from 'src/classes/message';
+import { EventType } from 'src/classes/eventType';
+import { ChatService } from '../services/Chat.service';
+import { SantaApiGetService } from '../services/santaApiService.service';
+import { GathererService } from '../services/gatherer.service';
+import { Client } from 'src/classes/client';
+import { MapService } from '../services/mapService.service';
 
 
 
@@ -9,11 +16,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CorrespondenceComponent implements OnInit {
 
-  constructor() { }
+  constructor(public SantaApiGet: SantaApiGetService,
+    public ChatService: ChatService,
+    public gatherer: GathererService,
+    public mapper: MapService) { }
 
-  displayedColumns: string[] = ['sender', 'recipient', 'event', 'contact'];
+  public allChats: Array<MessageHistory> = []
+  public unreadChats: Array<MessageHistory> = []
+  public eventChats: Array<MessageHistory> = []
+  public generalChats: Array<MessageHistory> = []
+  public events: Array<EventType> = []
 
-  ngOnInit() {
+  public showClientCard: boolean = false;
+
+  public selectedAnon: Client;
+
+
+  public async ngOnInit() {
+    this.ChatService.allChats.subscribe((historyArray: Array<MessageHistory>) => {
+      this.allChats = historyArray;
+    });
+    this.ChatService.allEventChats.subscribe((historyArray: Array<MessageHistory>) => {
+      this.eventChats = historyArray;
+    });
+    this.ChatService.allGeneralChats.subscribe((historyArray: Array<MessageHistory>) => {
+      this.generalChats = historyArray;
+    });
+    this.ChatService.allUnreadChats.subscribe((historyArray: Array<MessageHistory>) => {
+      this.unreadChats = historyArray;
+    });
+    this.gatherer.allEvents.subscribe((eventArray: Array<EventType>) => {
+      this.events = eventArray;
+    });
+
+    await this.gatherer.gatherAllEvents();
+    await this.gatherAllChats();
   }
+  public async gatherAllChats()
+  {
+    await this.ChatService.gatherAllChats();
+    await this.ChatService.gatherEventChats();
+    await this.ChatService.gatherGeneralChats();
+    await this.ChatService.gatherUnreadChats();
+  }
+  public sortByEvent(eventType: EventType)
+  {   
+    return this.eventChats.filter((message: MessageHistory) => {
+      return message.eventType.eventTypeID == eventType.eventTypeID;
+    });
+  }
+  public hideClientWindow()
+  {
 
+  }
+  public async populateSelectAnonCard(meta: ClientMeta)
+  {
+    this.selectedAnon = this.mapper.mapClient(await this.SantaApiGet.getClient(meta.clientID).toPromise());
+  }
+  async updateSelectedClient(clientID: string)
+  {
+    this.selectedAnon = this.mapper.mapClient(await this.SantaApiGet.getClient(clientID).toPromise());
+  }
 }
