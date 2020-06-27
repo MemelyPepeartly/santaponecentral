c55c82b18d9ae451a6d07b8cd53783e8e28646a8
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-27 06:32:49 -0400

    history object updated with new values for front

--- santa/src/Santa.Logic/Objects/MessageHistory.cs
+++ santa/src/Santa.Logic/Objects/MessageHistory.cs
@@ -8,14 +8,16 @@ namespace Santa.Logic.Objects
     {
         public List<Message> history { get; set; }
         public Guid? relationXrefID { get; set; }
+        public ClientMeta conversationClient { get; set; }
         public Event eventType { get; set; }
         /// <summary>
         /// Meta information for the person sending the gift if it is a history with a relationXrefID
         /// </summary>
-        public MessageClientMeta eventSenderClient { get; set; }
+        public ClientMeta eventSenderClient { get; set; }
         /// <summary>
         /// Meta information for the person recieving the gift if it is a history with a relationXrefID
         /// </summary>
-        public MessageClientMeta eventRecieverClient { get; set; }
+        public ClientMeta eventRecieverClient { get; set; }
+
     }
 }
