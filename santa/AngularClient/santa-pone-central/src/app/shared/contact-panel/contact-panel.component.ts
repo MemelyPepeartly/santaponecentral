520c1a8b4e5e19173d1a8c89b3002402be5fb1f9
TheMemelyMan <memelypepeartly@gmail.com>
2020-06-27 10:15:15 -0400

    contact panel moved to shared

---
+++ santa/AngularClient/santa-pone-central/src/app/shared/contact-panel/contact-panel.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Message, MessageHistory, ClientMeta } from 'src/classes/message';
+import { MapService, MapResponse } from 'src/app/services/mapService.service';
+import { SantaApiPostService } from 'src/app/services/santaApiService.service';
+import { ProfileRecipient, Profile } from 'src/classes/profile';
+import { AuthService } from 'src/app/auth/auth.service';
+import { ProfileService } from 'src/app/services/Profile.service';
+
+@Component({
+  selector: 'app-contact-panel',
+  templateUrl: './contact-panel.component.html',
+  styleUrls: ['./contact-panel.component.css']
+})
+export class ContactPanelComponent implements OnInit, AfterViewChecked {
+
+  constructor(public SantaApiPost: SantaApiPostService, public responseMapper: MapResponse, public auth: AuthService, public profileService: ProfileService) { }
+
+  @Input() selectedHistory: MessageHistory;
+  @Input() clientMeta: ClientMeta
+  
+  @ViewChild('chatFrame', {static: false}) chatFrame: ElementRef;
+
+  public isAdmin: boolean;
+
+
+  ngOnInit(): void {
+    this.auth.isAdmin.subscribe((admin: boolean) => {
+      this.isAdmin = admin;
+    });
+  }
+  ngAfterViewChecked() {        
+    this.scrollToBottom();      
+  } 
+  
+  public async scrollToBottom()
+  {
+    try 
+    {
+      this.chatFrame.nativeElement.scrollTop = this.chatFrame.nativeElement.scrollHeight;
+    }
+    catch(err) { }
+  }
+}
