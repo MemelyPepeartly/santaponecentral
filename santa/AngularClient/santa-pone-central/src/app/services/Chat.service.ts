57227f0d565c781255c6dfad4b983b9845f2591e
TheMemelyMan <memelypeperartly@gmail.com>
2020-06-27 08:22:36 -0400

    removed unneded things in chat service

--- santa/AngularClient/santa-pone-central/src/app/services/Chat.service.ts
+++ santa/AngularClient/santa-pone-central/src/app/services/Chat.service.ts
@@ -13,14 +13,10 @@ export class ChatService {
 
   public gettingAllChats: boolean = false;
   public gettingAllEventChats: boolean = false;
-  public gettingAllGeneralChats: boolean = false;
-  public gettingAllUnreadChats: boolean = false;
 
 
   private _allChats: BehaviorSubject<Array<MessageHistory>>= new BehaviorSubject([]);
   private _allEventChats: BehaviorSubject<Array<MessageHistory>>= new BehaviorSubject([]);
-  private _allGeneralChats: BehaviorSubject<Array<MessageHistory>>= new BehaviorSubject([]);
-  private _allUnreadChats: BehaviorSubject<Array<MessageHistory>>= new BehaviorSubject([]);
 
   // All Chats
   get allChats()
@@ -42,26 +38,6 @@ export class ChatService {
     this._allEventChats.next(messageHistories);
   }
 
-  // All General Chats
-  get allGeneralChats()
-  {
-    return this._allGeneralChats.asObservable();
-  
-  }
-  private updateAllGeneralChats(messageHistories: Array<MessageHistory>)
-  {
-    this._allGeneralChats.next(messageHistories);
-  }
-
-  // All Unread Chats
-  get allUnreadChats()
-  {
-    return this._allUnreadChats.asObservable();
-  }
-  private updateAllUnreadChats(messageHistories: Array<MessageHistory>)
-  {
-    this._allUnreadChats.next(messageHistories);
-  }
 
   // * GATHERING METHODS * //
   public async gatherAllChats()
@@ -92,24 +68,4 @@ export class ChatService {
       this.gettingAllEventChats = false;
     }, err => {console.log(err); this.gettingAllEventChats = false;}); 
   }
-  public async gatherGeneralChats()
-  {
-    
-  }
-  public async gatherUnreadChats()
-  {
-    this.gettingAllUnreadChats = true;
-    let historyArray: Array<MessageHistory> = [];
-
-    this.SantaApiGet.getMessageHistoriesWithUnreadMessages().subscribe(res => {
-      for(let i = 0; i < res.length; i++)
-      {
-        historyArray.push(this.ApiMapper.mapMessageHistory(res[i]))
-      }
-      this.updateAllUnreadChats(historyArray);
-      this.gettingAllUnreadChats = false;
-
-    }, err => {console.log(err); this.gettingAllUnreadChats = false;}); 
-  }
-
 }
