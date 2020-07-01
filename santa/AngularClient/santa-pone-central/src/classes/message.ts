8719d6b7bc60161552ff8b569194c11355e078fb
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-01 09:29:48 -0400

    better unread count math

--- santa/AngularClient/santa-pone-central/src/classes/message.ts
+++ santa/AngularClient/santa-pone-central/src/classes/message.ts
@@ -4,42 +4,57 @@ import { Client } from './client';
 
 //Message History class
 export class MessageHistory {
-    history: Array<Message>;
+    history: Array<Message> = [];
     relationXrefID: string;
     conversationClient: ClientMeta;
     eventType: EventType;
     eventSenderClient: ClientMeta;
     eventRecieverClient: ClientMeta;
 
-    get adminUnreadCount()
+    get adminUnreadCount() : number
     {
-        var count = 0;
-        this.history.forEach((message: Message) => {
-            if(message.isMessageRead == false && message.senderClient.clientID != null)
-            {
-                count += 1;
-            }
-        });
-        return count;
+        
+        if(this.history == undefined || this.history == null || this.history.length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            var count = 0;
+            this.history.forEach((message: Message) => {
+                if(message.isMessageRead == false && message.senderClient.clientID != null)
+                {
+                    count += 1;
+                }
+            });
+            return count;
+        }
     }
-    get memberUnreadCount()
+    get memberUnreadCount() : number
     {
-        var count = 0;
-        this.history.forEach((message: Message) => {
-            if(message.isMessageRead == false && message.recieverClient.clientID != null)
-            {
-                count += 1;
-            }
-        });
-        return count;
+        
+        if(this.history == undefined || this.history == null || this.history.length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            var count = 0;
+            this.history.forEach((message: Message) => {
+                if(message.isMessageRead == false && message.recieverClient.clientID != null)
+                {
+                    count += 1;
+                }
+            });
+            return count;
+        }
     }
-
 }
 // Message class for correspondance
 export class Message {
     chatMessageID: string;
-    senderClient: ClientMeta;
-    recieverClient: ClientMeta;
+    senderClient: ClientMeta = new ClientMeta();
+    recieverClient: ClientMeta = new ClientMeta();
     clientRelationXrefID?: string;
     messageContent: string;
     dateTimeSent: Date = new Date();
