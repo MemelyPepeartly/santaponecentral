63e4ede8b90c91343a63ffc653c2a92f185ae6c9
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-06 18:35:25 -0400

    Added new auth helper

---
+++ santa/src/Santa.Api/AuthHelper/AuthHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+using Santa.Api.Models.Auth0_Response_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Santa.Api.AuthHelper
+{
+    public class AuthHelper : IAuthHelper
+    {
+        private IConfigurationRoot ConfigRoot;
+
+        public AuthHelper(IConfiguration configRoot)
+        {
+            ConfigRoot = (IConfigurationRoot)configRoot;
+        }
+        public Auth0TokenModel getTokenModel()
+        {
+            string authClientID = ConfigRoot["Auth0API:client_id"];
+            string authClientSecret = ConfigRoot["Auth0API:Auth0Client_secret"];
+
+
+            var tokenRestClient = new RestClient("https://memelydev.auth0.com/oauth/token");
+            var tokenRequest = new RestRequest(Method.POST);
+            tokenRequest.AddHeader("content-type", "application/json");
+            tokenRequest.AddParameter("application/json", "{\"client_id\":\"" + authClientID + "\",\"client_secret\":\"" + authClientSecret + "\",\"audience\":\"https://memelydev.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            IRestResponse tokenResponse = tokenRestClient.Execute(tokenRequest);
+            Auth0TokenModel token = JsonConvert.DeserializeObject<Auth0TokenModel>(tokenResponse.Content.ToString());
+
+            return token;
+        }
+        public Auth0UserInfo getAuthClient(string authUserID)
+        {
+            var userRestClient = new RestClient("https://memelydev.auth0.com/api/v2/users/auth0|5f026b40b07e900019ebd131");
+            var userRequest = new RestRequest(Method.GET);
+            userRequest.AddHeader("authorization", "Bearer " + getTokenModel().access_token);
+            IRestResponse response = userRestClient.Execute(userRequest);
+            Auth0UserInfo user = JsonConvert.DeserializeObject<Auth0UserInfo>(response.Content.ToString());
+
+            return user;
+        }
+    }
+}
---
+++ santa/src/Santa.Api/AuthHelper/IAuthHelper.cs
@@ -0,0 +1,14 @@
+﻿using Santa.Api.Models.Auth0_Response_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.AuthHelper
+{
+    public interface IAuthHelper
+    {
+        Auth0TokenModel getTokenModel();
+        Auth0UserInfo getAuthClient(string authUserID);
+    }
+}
