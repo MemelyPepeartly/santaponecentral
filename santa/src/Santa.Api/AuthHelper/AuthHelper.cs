789adf86ae39ea9d909c62b974f2a572e59e7632
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-07 11:20:50 -0400

    debug and fix automatic auth0 user creation

--- santa/src/Santa.Api/AuthHelper/AuthHelper.cs
+++ santa/src/Santa.Api/AuthHelper/AuthHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using RestSharp;
 using Santa.Api.Models.Auth0_Response_Models;
@@ -12,67 +13,17 @@ namespace Santa.Api.AuthHelper
     public class AuthHelper : IAuthHelper
     {
         private IConfigurationRoot ConfigRoot;
+        private const string endpoint = "https://memelydev.auth0.com/api/v2/";
 
         public AuthHelper(IConfiguration configRoot)
         {
             ConfigRoot = (IConfigurationRoot)configRoot;
         }
-        public async Task<Auth0TokenModel> getTokenModel()
-        {
-            string authClientID = ConfigRoot["Auth0API:client_id"];
-            string authClientSecret = ConfigRoot["Auth0API:Auth0Client_secret"];
 
-
-            RestClient tokenRestClient = new RestClient("https://memelydev.auth0.com/oauth/token");
-            RestRequest tokenRequest = new RestRequest(Method.POST);
-            tokenRequest.AddHeader("content-type", "application/json");
-            tokenRequest.AddParameter("application/json", "{\"client_id\":\"" + authClientID + "\",\"client_secret\":\"" + authClientSecret + "\",\"audience\":\"https://memelydev.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
-            IRestResponse tokenResponse = await tokenRestClient.ExecuteAsync(tokenRequest);
-            Auth0TokenModel token = JsonConvert.DeserializeObject<Auth0TokenModel>(tokenResponse.Content.ToString());
-
-            return token;
-        }
-        public async Task<Auth0UserInfoModel> getAuthClientByID(string authUserID)
-        {
-            RestClient userRestClient = new RestClient("https://memelydev.auth0.com/api/v2/users/" + authUserID);
-            RestRequest userRequest = new RestRequest(Method.GET);
-            Auth0TokenModel token = await getTokenModel();
-            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
-            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
-            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
-
-            return user;
-        }
-
-        public async Task<Auth0UserInfoModel> getAuthClientByEmail(string authUserEmail)
-        {
-            RestClient userRestClient = new RestClient("https://memelydev.auth0.com/api/v2/users-by-email?email=" + authUserEmail);
-            RestRequest userRequest = new RestRequest(Method.GET);
-            Auth0TokenModel token = await getTokenModel();
-            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
-            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
-            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
-
-            return user;
-        }
-
-        public async Task<Auth0UserInfoModel> changeAuthClientPassword(string authUserID, Auth0UserPasswordModel passwordModel)
-        {
-            RestClient userRestClient = new RestClient("https://memelydev.auth0.com/api/v2/users/" + authUserID);
-            RestRequest userRequest = new RestRequest(Method.POST);
-            Auth0TokenModel token = await getTokenModel();
-            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
-            userRequest.AddJsonBody(passwordModel);
-            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
-            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
-
-            return user;
-
-        }
-
+        #region User Info Model
         public async Task<Auth0UserInfoModel> createAuthClient(string authEmail)
         {
-            RestClient userRestClient = new RestClient("https://memelydev.auth0.com/api/v2/users");
+            RestClient userRestClient = new RestClient(endpoint + "users");
             RestRequest userRequest = new RestRequest(Method.POST);
             Auth0TokenModel token = await getTokenModel();
             userRequest.AddHeader("authorization", "Bearer " + token.access_token);
@@ -89,5 +40,115 @@ namespace Santa.Api.AuthHelper
 
             return user;
         }
+        public async Task<Auth0UserInfoModel> getAuthClientByID(string authUserID)
+        {
+            RestClient userRestClient = new RestClient(endpoint + "users/" + authUserID);
+            RestRequest userRequest = new RestRequest(Method.GET);
+            Auth0TokenModel token = await getTokenModel();
+            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
+            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
+
+            return user;
+        }
+
+        public async Task<Auth0UserInfoModel> getAuthClientByEmail(string authUserEmail)
+        {
+            RestClient userRestClient = new RestClient(endpoint + "users-by-email?email=" + authUserEmail);
+            RestRequest userRequest = new RestRequest(Method.GET);
+            Auth0TokenModel token = await getTokenModel();
+            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
+            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
+
+            return user;
+        }
+
+        public async Task<Auth0UserInfoModel> updateAuthClientPassword(string authUserID, Auth0UserPasswordModel passwordModel)
+        {
+            RestClient userRestClient = new RestClient(endpoint + "users/" + authUserID);
+            RestRequest userRequest = new RestRequest(Method.POST);
+            Auth0TokenModel token = await getTokenModel();
+            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            userRequest.AddJsonBody(passwordModel);
+            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
+            Auth0UserInfoModel user = JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content.ToString());
+
+            return user;
+
+        }
+        public async Task updateAuthClientRole(string authUserID, string authRoleID)
+        {
+            RestClient userRestClient = new RestClient(endpoint + "users/" + authUserID + "/roles");
+            RestRequest userRequest = new RestRequest(Method.POST);
+            Auth0TokenModel token = await getTokenModel();
+
+            Auth0AddRoleIDModel responseModel = new Auth0AddRoleIDModel()
+            {
+                roles = new List<string>()
+            };
+            responseModel.roles.Add(authRoleID);
+
+            userRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            userRequest.AddJsonBody(responseModel);
+            IRestResponse response = await userRestClient.ExecuteAsync(userRequest);
+
+            if(response.IsSuccessful)
+            {
+                return;
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+        #endregion
+
+        #region Roles
+        public async Task<List<Auth0RoleModel>> getAllAuthRoles()
+        {
+            RestClient roleRestClient = new RestClient(endpoint + "roles");
+            RestRequest roleRequest = new RestRequest(Method.GET);
+            Auth0TokenModel token = await getTokenModel();
+            roleRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            IRestResponse response = await roleRestClient.ExecuteAsync(roleRequest);
+
+            List<Auth0RoleModel> roles = JsonConvert.DeserializeObject<List<Auth0RoleModel>>(response.Content.ToString());
+
+            return roles;
+        }
+
+        public async Task<Auth0RoleModel> getAuthRole(string authRoleID)
+        {
+            RestClient roleRestClient = new RestClient(endpoint + "roles/" + authRoleID);
+            RestRequest roleRequest = new RestRequest(Method.GET);
+            Auth0TokenModel token = await getTokenModel();
+            roleRequest.AddHeader("authorization", "Bearer " + token.access_token);
+            IRestResponse response = await roleRestClient.ExecuteAsync(roleRequest);
+            Auth0RoleModel role = JsonConvert.DeserializeObject<Auth0RoleModel>(response.Content.ToString());
+
+            return role;
+
+        }
+        #endregion
+
+        #region Utility
+        public async Task<Auth0TokenModel> getTokenModel()
+        {
+            string authClientID = ConfigRoot["Auth0API:client_id"];
+            string authClientSecret = ConfigRoot["Auth0API:Auth0Client_secret"];
+
+
+            RestClient tokenRestClient = new RestClient("https://memelydev.auth0.com/oauth/token");
+            RestRequest tokenRequest = new RestRequest(Method.POST);
+            tokenRequest.AddHeader("content-type", "application/json");
+            tokenRequest.AddParameter("application/json", "{\"client_id\":\"" + authClientID + "\",\"client_secret\":\"" + authClientSecret + "\",\"audience\":\"https://memelydev.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
+            IRestResponse tokenResponse = await tokenRestClient.ExecuteAsync(tokenRequest);
+            Auth0TokenModel token = JsonConvert.DeserializeObject<Auth0TokenModel>(tokenResponse.Content.ToString());
+
+            return token;
+        }
+        #endregion
     }
 }
--- santa/src/Santa.Api/AuthHelper/IAuthHelper.cs
+++ santa/src/Santa.Api/AuthHelper/IAuthHelper.cs
@@ -8,14 +8,21 @@ namespace Santa.Api.AuthHelper
 {
     public interface IAuthHelper
     {
-        #region Utility
-        Task<Auth0TokenModel> getTokenModel();
-        #endregion
         #region User Info Model
         Task<Auth0UserInfoModel> createAuthClient(string authEmail);
         Task<Auth0UserInfoModel> getAuthClientByID(string authUserID);
         Task<Auth0UserInfoModel> getAuthClientByEmail(string authUserEmail);
-        Task<Auth0UserInfoModel> changeAuthClientPassword(string authUserID, Auth0UserPasswordModel passwordModel);
+        Task<Auth0UserInfoModel> updateAuthClientPassword(string authUserID, Auth0UserPasswordModel passwordModel);
+        Task updateAuthClientRole(string authUserID, string authRoleID);
+        #endregion
+
+        #region Roles
+        Task<List<Auth0RoleModel>> getAllAuthRoles();
+        Task<Auth0RoleModel> getAuthRole(string authRoleID);
+        #endregion
+
+        #region Utility
+        Task<Auth0TokenModel> getTokenModel();
         #endregion
     }
 }
