6632db441e611181eb6bed620853daf4d4964174
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-07 13:19:28 -0400

    mailbag functinality added

---
+++ santa/src/Santa.Api/Mailbag/IMailbag.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using Santa.Api.Models.Auth0_Response_Models;
+using Santa.Api.Models.Mailbag_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.SendGrid
+{
+    public interface IMailbag
+    {
+        MailbagKeyModel getKey();
+        Task sendPasswordResetEmail(Logic.Objects.Client recipient, Auth0TicketResponse ticket);
+        Task sendChatNotificationEmail(Logic.Objects.Client recipient, Message message);
+    }
+}
