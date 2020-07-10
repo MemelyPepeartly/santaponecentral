6bb7bad6e2f3cc31775162a5a569825da4a3892a
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-09 23:46:34 -0400

    pushing changes to prepare for angular upgrade

--- santa/AngularClient/santa-pone-central/src/app/app-routing.module.ts
+++ santa/AngularClient/santa-pone-central/src/app/app-routing.module.ts
@@ -2,22 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { UsernavComponent } from './navbar/usernav/usernav.component';
-import { LoginComponent } from './home/login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { AdminnavComponent } from './navbar/adminnav/adminnav.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeadquartersComponent } from './headquarters/headquarters.component';
 import { AdminHelpComponent } from './admin-help/admin-help.component';
-import { AdminControlComponent } from './admin-control/admin-control.component';
 import { CorrespondenceComponent } from './correspondence/correspondence.component';
+import { AdminControlComponent } from './admin-control/admin-control.component';
 
 
 //Auth imports
 import { AuthGuard } from './auth/auth.guard'
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorService } from './auth/interceptor.service';
+
 
 const appRoutes: Routes = [
   { path: 'profile',
@@ -62,7 +59,8 @@ const appRoutes: Routes = [
   { 
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full' }
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
