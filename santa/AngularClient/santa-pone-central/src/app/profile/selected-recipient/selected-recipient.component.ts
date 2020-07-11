367792124925b5ec507624f46ae576e6b1a48e8c
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-11 19:32:23 -0400

    selected recipient component display functional

--- santa/AngularClient/santa-pone-central/src/app/profile/selected-recipient/selected-recipient.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/selected-recipient/selected-recipient.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProfileRecipient } from 'src/classes/profile';
 import { ProfileService } from 'src/app/services/Profile.service';
+import { Survey } from 'src/classes/survey';
+import { GathererService } from 'src/app/services/gatherer.service';
+import { EventType } from 'src/classes/eventType';
 
 @Component({
   selector: 'app-selected-recipient',
@@ -9,11 +12,17 @@ import { ProfileService } from 'src/app/services/Profile.service';
 })
 export class SelectedRecipientComponent implements OnInit {
 
-  constructor(public profileService: ProfileService) { }
+  constructor(public profileService: ProfileService,
+    public gatherer: GathererService) { }
 
   @Input() selectedRecipient: ProfileRecipient;
+  public events: Array<EventType> = []
 
   ngOnInit(): void {
+    this.gatherer.allEvents.subscribe((eventArray: Array<EventType>) => {
+      this.events = eventArray;
+    });
+    this.gatherer.gatherAllEvents();
   }
 
 }
