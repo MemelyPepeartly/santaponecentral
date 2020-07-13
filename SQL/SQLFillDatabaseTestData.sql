cfbdb34b2ded9f04d8270deffe3695011b33ad7e
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-13 02:20:07 -0400

    updated web.config and test data

--- SQL/SQLFillDatabaseTestData.sql
+++ SQL/SQLFillDatabaseTestData.sql
@@ -157,9 +157,9 @@ VALUES
 INSERT INTO app.Client (clientID, clientStatusID, clientName, nickname, email, addressLine1, addressLine2, city, [state], postalCode, country)
 VALUES
     (@client1IDGUID, @statusID2GUID, 'Wobble Wub', 'Wibble Wab', 'test@test.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country'),
-    (@client2IDGUID, @statusID1GUID, 'Evershade Pensive', 'Sharona Virus', 'memelypepeartly@gmail.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country'),
+    (@client2IDGUID, @statusID1GUID, 'Evershade Pensive', 'Sharona Virus', 'sorengylfietwilightdigger@gmail.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country'),
     (@client3IDGUID, @statusID2GUID, 'Venport Measure', 'Picky Wikket', 'moox100@gmail.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country'),
-    (@client4IDGUID, @statusID2GUID, 'Memely Pepeartly', 'Duk', 'sorengylfietwilightdigger@gmail.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country');
+    (@client4IDGUID, @statusID2GUID, 'Memely Pepeartly', 'Duk', 'memelypepeartly@gmail.com', 'Address 1', 'Address 2', 'City', 'State', 'Postal Code', 'Country');
 
 
 -- INSERT INTO app.SurveyResponse (surveyResponseID, surveyID, clientID, surveyQuestionID, surveyOptionID, responseText)
