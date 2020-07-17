99ea7790152ea6c3d15824acc109473b053411d9
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-16 20:00:43 -0400

    auth0 model and type update

--- santa/src/Santa.Api/Models/Auth0 Response Models/Auth0UserInfoModel.cs
+++ santa/src/Santa.Api/Models/Auth0 Response Models/Auth0UserInfoModel.cs
@@ -8,11 +8,18 @@ namespace Santa.Api.Models.Auth0_Response_Models
     public class Auth0UserInfoModel
     {
         public DateTime created_at { get; set; }
-        public DateTime updated_at { get; set; }
         public string email { get; set; }
-        public string email_verified { get; set; }
+        public bool email_verified { get; set; }
+        public List<Auth0IdentityModel> identities { get; set; }
         public string name { get; set; }
         public string nickname { get; set; }
+        public string picture { get; set; }
+        public DateTime updated_at { get; set; }
         public string user_id { get; set; }
+        public DateTime last_password_reset { get; set; }
+        public string last_ip { get; set; }
+        public DateTime last_login { get; set; }
+        public int logins_count { get; set; }
+
     }
 }
