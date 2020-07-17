81ac86e792748ac568b195a8fd1dc5396619fdd1
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-16 20:00:52 -0400

    model update

---
+++ santa/src/Santa.Api/Models/Auth0 Response Models/Auth0ChangeEmailModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Auth0_Response_Models
+{
+    public class Auth0ChangeEmailModel
+    {
+        public string email { get; set; }
+        public string name { get; set; }
+    }
+}
