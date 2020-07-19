223687cb5af6fc36a3782dd11893a2f08a548c27
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-18 23:58:01 -0400

    modified archetecture to allow multiple recipients

---
+++ santa/src/Santa.Api/Models/Client Models/ApiClientRelationships.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Client_Models
+{
+    public class ApiClientRelationships
+    {
+        public List<ApiClientRelationship> assignments { get; set; }
+    }
+}
