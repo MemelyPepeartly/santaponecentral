8d32dae63f0dd33a9a773bf9a98c82b059afb290
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-20 04:35:39 -0400

    buh

--- santa/src/Santa.Api/Startup.cs
+++ santa/src/Santa.Api/Startup.cs
@@ -45,7 +45,7 @@ namespace Santa.Api
                 options.AddPolicy("AllowAngular",
                 builder =>
                 {
-                    builder.WithOrigins("http://localhost:4200", "https://dev-santaponecentral.azurewebsites.net", "https://www.santaponecentral.net")
+                    builder.WithOrigins("http://localhost:4200", "https://dev-santaponecentral.azurewebsites.net", "https://www.santaponecentral.net", "https://santaponecentral.azurewebsites.net")
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials();
