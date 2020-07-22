404f4411302b76be740cb10b5511da3cf63410e3
TheMemelyMan <memelypeperartly@gmail.com>
2020-07-22 16:23:36 -0400

    generated new component

---
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/completed-anons/completed-anons.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { GathererService } from 'src/app/services/gatherer.service';
+import { Client } from 'src/classes/client';
+
+@Component({
+  selector: 'app-completed-anons',
+  templateUrl: './completed-anons.component.html',
+  styleUrls: ['./completed-anons.component.css']
+})
+export class CompletedAnonsComponent implements OnInit {
+
+  constructor(public gatherer: GathererService) { }
+
+  @Input() completedClients: Array<Client> = [];
+
+  @Output() clickedClient: EventEmitter<any> = new EventEmitter();
+  actionTaken: boolean = false;
+  showSpinner: boolean = false;
+
+  ngOnInit(): void {
+  }
+  showCardInfo(client)
+  {
+    this.clickedClient.emit(client);
+  }
+  public async refreshIncomingClientList()
+  {
+    if(this.actionTaken)
+    {
+      await this.gatherer.gatherAllClients();
+      this.actionTaken = false;
+      this.showSpinner = false;
+    }
+  }
+  setAction(event: boolean)
+  {
+    this.actionTaken = event;
+  }
+  manualRefresh()
+  {
+    this.actionTaken = true;
+    this.showSpinner = true;
+    this.refreshIncomingClientList();
+  }
+
+}
