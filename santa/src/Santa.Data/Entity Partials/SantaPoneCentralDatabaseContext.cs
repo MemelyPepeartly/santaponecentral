94e00d0e3e991ea28407079541c9e080ebd280a1
TheMemelyMan <memelypepeartly@gmail.com>
2020-07-30 04:02:32 -0400

    new partial thing

---
+++ santa/src/Santa.Data/Entity Partials/SantaPoneCentralDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Data.Entities
+{
+    public partial class SantaPoneCentralDatabaseContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
