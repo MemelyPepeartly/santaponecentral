7c7c4e5e65ffeab2081fe72a5d3aad6285aae7fe
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-08 10:46:42 -0400

    can select clients by chips now

--- santa/AngularClient/santa-pone-central/src/app/admin-control/auto-assignment/auto-assignment.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/admin-control/auto-assignment/auto-assignment.component.ts
@@ -3,7 +3,8 @@ import { Client } from 'src/classes/client';
 import { Tag } from 'src/classes/tag';
 import { TagConstants } from '../../shared/constants/tagConstants.enum'
 import { GathererService } from 'src/app/services/gatherer.service';
-import { SantaApiPostService } from 'src/app/services/santaApiService.service';
+import { SantaApiPostService, SantaApiGetService } from 'src/app/services/santaApiService.service';
+import { MapService } from 'src/app/services/mapService.service';
 
 @Component({
   selector: 'app-auto-assignment',
@@ -11,16 +12,24 @@ import { SantaApiPostService } from 'src/app/services/santaApiService.service';
   styleUrls: ['./auto-assignment.component.css']
 })
 export class AutoAssignmentComponent implements OnInit {
-  constructor(public gatherer: GathererService, public SantaApiPost: SantaApiPostService) { }
+  constructor(public gatherer: GathererService,
+    public SantaApiPost: SantaApiPostService,
+    public SantaApiGet: SantaApiGetService,
+    public mapper: MapService) { }
 
   @Input() allClients: Array<Client> = []
+
+  public logArray: Array<string> = []
+
+  public selectedClient: Client = new Client();
 
   public gatheringAllClients: boolean = false;
   public postingNewAssignments: boolean = false;
 
-  public buttonClicked: boolean;
+  public buttonClicked: boolean = false;
+  public refreshList: boolean = false;
 
-  public logArray: Array<string> = []
+  public showClientCard: boolean = false;
 
   ngOnInit(): void {
     this.buttonClicked = false;
@@ -49,5 +58,26 @@ export class AutoAssignmentComponent implements OnInit {
 
     this.postingNewAssignments = false;
   }
-
+  public async updateSelectedClient(clientID: string)
+  {
+    this.selectedClient = this.mapper.mapClient(await this.SantaApiGet.getClientByClientID(clientID).toPromise());
+  }
+  public selectClient(client: Client)
+  {
+    this.selectedClient = client;
+    this.showClientCard = true;
+  }
+  public async hideClientWindow()
+  {
+    this.showClientCard = false;
+    this.selectedClient = new Client();
+    if(this.refreshList)
+    {
+      await this.gatherer.gatherAllClients();
+    }
+  }
+  public setRefreshListAction(event: boolean)
+  {
+    this.refreshList = event;
+  }
 }
