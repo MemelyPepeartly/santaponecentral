f32a4f6fbe116906d864bff51ce1be394a21f40b
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-08 03:52:53 -0400

    auto assign fully functional

--- santa/AngularClient/santa-pone-central/src/app/admin-control/auto-assignment/auto-assignment.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/admin-control/auto-assignment/auto-assignment.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Client } from 'src/classes/client';
+import { Tag } from 'src/classes/tag';
+import { TagConstants } from '../../shared/constants/tagConstants.enum'
+import { GathererService } from 'src/app/services/gatherer.service';
+import { SantaApiPostService } from 'src/app/services/santaApiService.service';
 
 @Component({
   selector: 'app-auto-assignment',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auto-assignment.component.css']
 })
 export class AutoAssignmentComponent implements OnInit {
+  constructor(public gatherer: GathererService, public SantaApiPost: SantaApiPostService) { }
 
-  constructor() { }
+  @Input() allClients: Array<Client> = []
+
+  public gatheringAllClients: boolean = false;
+  public postingNewAssignments: boolean = false;
+
+  public buttonClicked: boolean;
+
+  public logArray: Array<string> = []
 
   ngOnInit(): void {
+    this.buttonClicked = false;
+    this.gatherer.gatheringAllClients.subscribe((status: boolean) => {
+      this.gatheringAllClients = status
+    });
+  }
+  public sortMassMailers() : Array<Client>
+  {
+    return this.allClients.filter((client: Client) => {return client.tags.some((tag: Tag) => {return tag.tagName == TagConstants.MASS_MAILER})})
+  }
+  public sortMassMailRecipients() : Array<Client>
+  {
+    return this.allClients.filter((client: Client) => {return client.tags.some((tag: Tag) => {return tag.tagName == TagConstants.MASS_MAIL_RECIPIENT})})
+  }
+  public async giveAssignments()
+  {
+    this.logArray = []
+    this.buttonClicked = true;
+    this.postingNewAssignments = true;
+
+    var logResponse = await this.SantaApiPost.postAutoAssignmentRequest().toPromise();
+    logResponse.forEach((entry:string) => {
+      this.logArray.push(entry)
+    });
+
+    this.postingNewAssignments = false;
   }
 
 }
