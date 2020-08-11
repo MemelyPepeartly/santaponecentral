5887bde8b94342cef83bdf4c0d60cc2199fe5827
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-11 10:01:25 -0400

    added sorting algorithm for questions comp

--- santa/AngularClient/santa-pone-central/src/app/admin-control/survey-control/survey-control.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/admin-control/survey-control/survey-control.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Survey } from 'src/classes/survey';
+import { Survey, Question } from 'src/classes/survey';
 import { Validators, FormControl, FormBuilder, FormGroup } from '@angular/forms';
 import { SantaApiGetService, SantaApiPutService, SantaApiPostService, SantaApiDeleteService } from 'src/app/services/santaApiService.service';
 import { MapResponse, MapService } from 'src/app/services/mapService.service';
@@ -23,6 +23,7 @@ export class SurveyControlComponent implements OnInit {
     public ApiMapper: MapService) { }
 
   @Input() allSurveys: Array<Survey> = []
+  @Input() allQuestions: Array<Question> = []
 
   public selectedSurvey: Survey
 
@@ -60,5 +61,16 @@ export class SurveyControlComponent implements OnInit {
   public addNewSurvey()
   {
 
+  }
+  public sortAddableQuestions() : Array<Question>
+  {
+    let questionsThatCanBeAdded: Array<Question>= []
+    this.allQuestions.forEach((question: Question) => {
+      if(!this.selectedSurvey.surveyQuestions.some((thing: Question) => {return thing.questionID == question.questionID}))
+      {
+        questionsThatCanBeAdded.push(question);
+      }
+    });
+    return questionsThatCanBeAdded;
   }
 }
