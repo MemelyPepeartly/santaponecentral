4025e255b9b4c97390bcf304949ab5cd80832ceb
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-10 23:52:06 -0400

    change address on profile till assigned functional

--- santa/AngularClient/santa-pone-central/src/app/profile/information/information.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/information/information.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Profile } from 'src/classes/profile';
 import { ProfileService } from 'src/app/services/Profile.service';
 import { EventType } from 'src/classes/eventType';
-import { ChangeSurveyResponseModel } from 'src/classes/responseTypes';
+import { ChangeSurveyResponseModel, ClientAddressResponse } from 'src/classes/responseTypes';
 import { SantaApiPutService } from 'src/app/services/santaApiService.service';
 import { SurveyResponse } from 'src/classes/survey';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CountriesService } from 'src/app/services/countries.service';
 
 @Component({
   selector: 'app-information',
@@ -14,7 +15,10 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class InformationComponent implements OnInit {
 
-  constructor(public profileService: ProfileService, public SantaApiPut: SantaApiPutService, private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    public profileService: ProfileService,
+    public SantaApiPut: SantaApiPutService,
+    public countryService: CountriesService) { }
 
   @Input() loading: boolean;
   @Input() profile: Profile;
@@ -22,9 +26,28 @@ export class InformationComponent implements OnInit {
   @Input() clientResponseFormGroup: FormGroup;
 
   public editingResponse: boolean;
+  public changingAddress: boolean;
+
+  public showAddressChangeForm: boolean;
+
+  public countries: Array<any> = [];
+  public clientAddressFormGroup: FormGroup;
+  get addressFormControls()
+  {
+    return this.clientAddressFormGroup.controls;
+  }
 
 
   ngOnInit(): void {
+    this.countries = this.countryService.allCountries();
+    this.clientAddressFormGroup = this.formBuilder.group({
+      addressLine1: ['', [Validators.required, Validators.pattern("[A-Za-z0-9 ]{1,50}"), Validators.maxLength(50)]],
+      addressLine2: ['', [Validators.pattern("[A-Za-z0-9 ]{1,50}"), Validators.maxLength(50)]],
+      city: ['', [Validators.required, Validators.pattern("[A-Za-z0-9 ]{1,50}"), Validators.maxLength(50)]],
+      state: ['', [Validators.required, Validators.pattern("[A-Za-z0-9 ]{1,50}"), Validators.maxLength(50)]],
+      postalCode: ['', [Validators.required, Validators.maxLength(25)]],
+      country: ['', Validators.required]
+    });
   }
   public async submitNewResponse(surveyResponseID: string)
   {
@@ -37,5 +60,24 @@ export class InformationComponent implements OnInit {
 
     this.editingResponse = false;
   }
+  public async submitNewAddress()
+  {
+    this.changingAddress = true;
 
+    let newAddressResponse = new ClientAddressResponse();
+
+    newAddressResponse.clientAddressLine1 = this.addressFormControls.addressLine1.value;
+    newAddressResponse.clientAddressLine2 = this.addressFormControls.addressLine2.value;
+    newAddressResponse.clientCity = this.addressFormControls.city.value;
+    newAddressResponse.clientState = this.addressFormControls.state.value;
+    newAddressResponse.clientCountry = this.addressFormControls.country.value;
+    newAddressResponse.clientPostalCode = this.addressFormControls.postalCode.value;
+
+    await this.SantaApiPut.putProfileAddress(this.profile.clientID, newAddressResponse).toPromise();
+    await this.profileService.getProfile(this.profile.email);
+    
+    this.clientAddressFormGroup.reset();
+    this.showAddressChangeForm = false;
+    this.changingAddress = false;
+  }
 }
