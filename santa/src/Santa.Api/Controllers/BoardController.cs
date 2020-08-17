13a9facc7a0bfd75970604092dbc85263dfe9a92
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-17 18:15:18 -0400

    Updated board controller with all implimented functionality

--- santa/src/Santa.Api/Controllers/BoardController.cs
+++ santa/src/Santa.Api/Controllers/BoardController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Santa.Api.Models.Board_Entry_Models;
+using Santa.Logic.Interfaces;
+using Santa.Logic.Objects;
 
 namespace Santa.Api.Controllers
 {
@@ -11,36 +14,169 @@ namespace Santa.Api.Controllers
     [ApiController]
     public class BoardController : ControllerBase
     {
-        // GET: api/Board
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IRepository repository;
+
+        public BoardController(IRepository _repository)
         {
-            return new string[] { "value1", "value2" };
+            repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
+        }
+
+        // GET: api/Board
+        /// <summary>
+        /// Gets a list of all the board entries posted
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<List<BoardEntry>>> GetAllEntries()
+        {
+            try
+            {
+                List<BoardEntry> logicBoardEntries = await repository.GetAllBoardEntriesAsync();
+                return Ok(logicBoardEntries);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
+
         }
 
         // GET: api/Board/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        /// <summary>
+        /// Gets a specific board entry by its board entry ID
+        /// </summary>
+        /// <param name="boardEntryID"></param>
+        /// <returns></returns>
+        [HttpGet("{boardEntryID}")]
+        public async Task<ActionResult<BoardEntry>> GetBoardEntryByID(Guid boardEntryID)
         {
-            return "value";
+            try
+            {
+                BoardEntry logicBoardEntry = await repository.GetBoardEntryByIDAsync(boardEntryID);
+                return logicBoardEntry;
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
+        }
+
+        // GET: api/Board/PostNumber/5
+        /// <summary>
+        /// Gets a board entry by it's post number
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("/PostNumber/{postNumber}")]
+        public async Task<ActionResult<BoardEntry>> GetBoardEntryByPostNumber(int postNumber)
+        {
+            try
+            {
+                BoardEntry logicBoardEntry = await repository.GetBoardEntryByPostNumberAsync(postNumber);
+                return logicBoardEntry;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
         }
 
         // POST: api/Board
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<BoardEntry>> PostNewBoardEntry([FromBody] NewBoardEntryModel model)
         {
+            try
+            {
+                BoardEntry newLogicBoardEntry = new BoardEntry()
+                {
+                    boardEntryID = Guid.NewGuid(),
+                    postNumber = model.postNumber,
+                    postDescription = model.postDescription
+                };
+                await repository.UpdateBoardEntryAsync(newLogicBoardEntry);
+                await repository.SaveAsync();
+
+                return Ok(await repository.GetBoardEntryByIDAsync(newLogicBoardEntry.boardEntryID));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
         }
 
-        // PUT: api/Board/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // PUT: api/Board/5/PostNumber
+        /// <summary>
+        /// Updates a board entry's post number by its boardEntryID
+        /// </summary>
+        /// <param name="boardEntryID"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut("{boardEntryID}/PostNumber")]
+        public async Task<ActionResult<BoardEntry>> PutPostNumber(Guid boardEntryID, [FromBody] EditPostNumberModel model)
         {
+            try
+            {
+                BoardEntry newLogicBoardEntry = new BoardEntry()
+                {
+                    boardEntryID = boardEntryID,
+                    postNumber = model.postNumber
+                };
+                await repository.UpdateBoardEntryAsync(newLogicBoardEntry);
+                await repository.SaveAsync();
+                return Ok(await repository.GetBoardEntryByIDAsync(boardEntryID));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
         }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // PUT: api/Board/5/PostDescription
+        /// <summary>
+        /// Updates a board entry's post description by its boardEntryID
+        /// </summary>
+        /// <param name="boardEntryID"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut("{boardEntryID}/PostDescription")]
+        public async Task<ActionResult<BoardEntry>> PutPostDescription(Guid boardEntryID, [FromBody] EditPostDescriptionModel model)
         {
+            try
+            {
+                BoardEntry newLogicBoardEntry = new BoardEntry()
+                {
+                    boardEntryID = boardEntryID,
+                    postDescription = model.postDescription
+                };
+                await repository.UpdateBoardEntryAsync(newLogicBoardEntry);
+                await repository.SaveAsync();
+                return Ok(await repository.GetBoardEntryByIDAsync(boardEntryID));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
+        }
+
+        // DELETE: api/Board/5
+        /// <summary>
+        /// Deletes a board entry by its ID
+        /// </summary>
+        /// <param name="boardEntryID"></param>
+        /// <returns></returns>
+        [HttpDelete("{boardEntryID}")]
+        public async Task<ActionResult> DeleteBoardEntry(Guid boardEntryID)
+        {
+            try
+            {
+                await repository.DeleteBoardEntryByIDAsync(boardEntryID);
+                await repository.SaveAsync();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.InnerException);
+            }
         }
     }
 }
