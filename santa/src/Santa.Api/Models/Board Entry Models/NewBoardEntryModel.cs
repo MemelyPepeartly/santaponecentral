3574330b2eebeab10301cc1ec4105ede620995c6
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-17 18:14:48 -0400

    New models for post and puts on board controller

---
+++ santa/src/Santa.Api/Models/Board Entry Models/NewBoardEntryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Board_Entry_Models
+{
+    /// <summary>
+    /// Model for posting new board entries
+    /// </summary>
+    public class NewBoardEntryModel
+    {
+        public int postNumber { get; set; }
+        public string postDescription { get; set; }
+    }
+}
