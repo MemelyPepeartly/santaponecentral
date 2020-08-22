889de305640b43be8d194a63d463b7dd17ae9dd1
TheMemelyMan <memelypeperartly@gmail.com>
2020-08-22 14:32:05 -0400

    fields and form vars added

--- santa/AngularClient/santa-pone-central/src/app/shared/mission-board-table/mission-board-table.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/shared/mission-board-table/mission-board-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { BoardEntry } from 'src/classes/missionBoards';
+import { BoardEntry, EntryType } from 'src/classes/missionBoards';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mission-board-table',
@@ -8,12 +9,31 @@ import { BoardEntry } from 'src/classes/missionBoards';
 })
 export class MissionBoardTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   @Input() boardEntries: Array<BoardEntry> = [];
+  @Input() entryType: EntryType = new EntryType();
+  @Input() allowForm: boolean;
+
+  public boardEntryFormGroup: FormGroup;
+  get boardEntryFormControls()
+  {
+    return this.boardEntryFormGroup.controls;
+  }
+
+  public showFormFields: boolean = false;
+  public postingEntry: boolean = false;
 
   columns: string[] = ["number", "description", "type"];
 
   ngOnInit(): void {
+    this.boardEntryFormGroup = this.formBuilder.group({
+      postNumber: ['', [Validators.required, Validators.maxLength(10), Validators.pattern("^[0-9]*$")]],
+      postDescription: ['', [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+  submitBoardEntry()
+  {
+
   }
 }
