3e4084d1d049ed1c50f71cb6bfb1ea993dd56248
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-04 11:23:24 -0400

    basic variable setup for manual signup

--- santa/AngularClient/santa-pone-central/src/app/headquarters/manual-add/manual-add.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/manual-add/manual-add.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { SantaApiGetService, SantaApiPostService } from 'src/app/services/santaApiService.service';
+import { GathererService } from 'src/app/services/gatherer.service';
+import { MapService, MapResponse } from 'src/app/services/mapService.service';
+import { CountriesService } from 'src/app/services/countries.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SurveyFormComponent } from 'src/app/signup/survey-form/survey-form.component';
 
 @Component({
   selector: 'app-manual-add',
@@ -7,7 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManualAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(public SantaGet: SantaApiGetService,
+    public SantaPost: SantaApiPostService,
+    public gatherer: GathererService,
+    public mapper: MapService,
+    public responseMapper: MapResponse,
+    public countryService: CountriesService,
+    private formBuilder: FormBuilder) { }
+
+  public clientInfoFormGroup: FormGroup;
+  public clientAddressFormGroup: FormGroup;
+  public clientEventFormGroup: FormGroup;
+  public surveyFormGroup: FormGroup;
+
+  @ViewChildren(SurveyFormComponent) surveyForms: QueryList<SurveyFormComponent>;
 
   ngOnInit(): void {
   }
