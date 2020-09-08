7f2f6fe578563fce4afbdc8e77efddb6499b5945
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-07 21:47:34 -0400

    setup for response component

--- santa/AngularClient/santa-pone-central/src/app/shared/response-list/response-list.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/shared/response-list/response-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Question, SurveyResponse, Survey } from 'src/classes/survey';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-response-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  @Input() survey: Survey = new Survey();
+  @Input() responses: Array<SurveyResponse> = [];
+  @Input() editable: boolean = false;
+
+  @Output() submitClickedEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() formsValidEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  public surveyFormGroup: FormGroup;
+
 
   ngOnInit(): void {
+    this.surveyFormGroup = this.formBuilder.group({});
+    this.addFields();
   }
-
+  addFields()
+  {
+    this.survey.surveyQuestions.forEach((question: Question) => {
+      this.surveyFormGroup.addControl(question.questionID, new FormControl('', [Validators.required, Validators.maxLength(2000)]))
+    });
+  }
 }
