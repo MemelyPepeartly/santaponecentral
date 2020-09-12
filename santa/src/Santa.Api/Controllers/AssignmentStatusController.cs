b50b4ba4ed471a8331aab60c00bcc33c798e340e
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-12 16:04:21 -0400

    Controller setup and implimentation

---
+++ santa/src/Santa.Api/Controllers/AssignmentStatusController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Santa.Logic.Interfaces;
+using Santa.Logic.Objects;
+
+namespace Santa.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssignmentStatusController : ControllerBase
+    {
+        private readonly IRepository repository;
+
+        public AssignmentStatusController(IRepository _repository)
+        {
+            repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
+        }
+
+        // GET: api/AssignmentStatus
+        [HttpGet]
+        public async Task<ActionResult<List<Logic.Objects.AssignmentStatus>>> GetAllAssignmentStatuses()
+        {
+            try
+            {
+                List<Logic.Objects.AssignmentStatus> logicListAssignmentStatuses = await repository.GetAllAssignmentStatuses();
+                return Ok(logicListAssignmentStatuses);
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        // GET: api/AssignmentStatus/5
+        [HttpGet("{assignmentStatusID}")]
+        public async Task<ActionResult<Logic.Objects.AssignmentStatus>> GetAssignmentStatusByID(Guid assignmentStatusID)
+        {
+            try
+            {
+                Logic.Objects.AssignmentStatus logicAssignmentStatus = await repository.GetAssignmentStatusByID(assignmentStatusID);
+                return Ok(logicAssignmentStatus);
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+        // GET: api/AssignmentStatus/GetAll/5
+        [HttpGet("GetAll/{assignmentStatusID}")]
+        public async Task<ActionResult<List<Logic.Objects.AssignmentStatus>>> GetAllAssignmentsWithStatusByID(Guid assignmentStatusID)
+        {
+            try
+            {
+                List<object> listLogicAssignmentStatuses = await repository.GetAssignmentsByAssignmentStatusID(assignmentStatusID);
+                return Ok(listLogicAssignmentStatuses);
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        // POST: api/AssignmentStatus
+        [HttpPost]
+        public async Task<ActionResult<Logic.Objects.AssignmentStatus>> PostNewAssignmentStatus([FromBody] string value)
+        {
+            try
+            {
+                AssignmentStatus targetAssignmentStatus = new AssignmentStatus();
+                await repository.UpdateAssignmentStatus(targetAssignmentStatus);
+                AssignmentStatus newAssignmentStatus = await repository.GetAssignmentStatusByID(targetAssignmentStatus.assignmentStatusID);
+
+                return Ok(newAssignmentStatus);
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        // PUT: api/AssignmentStatus/5
+        [HttpPut("{assignmentStatusID}")]
+        public async Task<ActionResult<Logic.Objects.AssignmentStatus>> PutAssignmentStatus(Guid assignmentStatusID, [FromBody] string value)
+        {
+            try
+            {
+                AssignmentStatus targetAssignmentStatus = new AssignmentStatus();
+                await repository.UpdateAssignmentStatus(targetAssignmentStatus);
+                AssignmentStatus updatedAssignmenStatus = await repository.GetAssignmentStatusByID(assignmentStatusID);
+
+                return Ok(updatedAssignmenStatus);
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{assignmentStatusID}")]
+        public async Task<ActionResult> DeleteAssignmentStatusByID(Guid assignmentStatusID)
+        {
+            try
+            {
+                await repository.DeleteAssignmentStatusByID(assignmentStatusID);
+                return Ok();
+            }
+            catch (ArgumentNullException e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
