27a1ea06ec8aff02c01f45813dad3c3bc6e5362d
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-13 04:36:39 -0400

    Profile model to include profile recipient assignment status types

--- santa/AngularClient/santa-pone-central/src/classes/profile.ts
+++ santa/AngularClient/santa-pone-central/src/classes/profile.ts
@@ -2,6 +2,7 @@ import { Status } from './status';
 import { Address } from './address';
 import { SurveyResponse } from './survey';
 import { EventType } from './eventType';
+import { AssignmentStatus } from './client';
 
 export class Profile
 {
@@ -22,6 +23,7 @@ export class ProfileRecipient
     clientName: string;
     clientNickname: string;
     address: Address = new Address();
+    assignmentStatus: AssignmentStatus = new AssignmentStatus();
     recipientEvent: EventType = new EventType();
     responses: Array<SurveyResponse> = [];
 }
