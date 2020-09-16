80acb83dd9a6db43496fe73e5bfbd930d2c01e82
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-16 14:48:10 -0400

    Input output mapping on assignments panel

--- santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/assignments-panel/assignments-panel.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/selected-anon/assignments-panel/assignments-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RelationshipMeta } from 'src/classes/client';
 
 @Component({
   selector: 'app-assignments-panel',
@@ -9,9 +10,15 @@ export class AssignmentsPanelComponent implements OnInit {
 
   constructor() { }
 
-  @Input()
+  @Input() senders: Array<RelationshipMeta> = [];
+  @Input() assignments: Array<RelationshipMeta> = [];
+
+  @Output() relationshipSelectedEvent: EventEmitter<RelationshipMeta> = new EventEmitter<RelationshipMeta>();
 
   ngOnInit(): void {
   }
-
+  public emitSelectedRelationship(relationship: RelationshipMeta)
+  {
+    this.relationshipSelectedEvent.emit(relationship);
+  }
 }
