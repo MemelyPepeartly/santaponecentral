3de58f9767ef5820691e35d8fcf3ff8be8a55505
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-17 19:30:47 -0400

    Potential search models

---
+++ santa/src/Santa.Api/Models/Search Models/ClientFilterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Search_Models
+{
+    public class ClientFilterModel
+    {
+        public List<Guid> tagQueries { get; set; }
+        public List<Guid> statusQueries { get; set; }
+        public List<string> nameQueries { get; set; }
+        public List<string> nicknameQueries { get; set; }
+        public List<int> senderCountQueries { get; set; }
+        public List<int> assignmentCountQueries { get; set; }
+    }
+}
