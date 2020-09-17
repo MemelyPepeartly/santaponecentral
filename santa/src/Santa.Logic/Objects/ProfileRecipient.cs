aa44cec5c1b7df9b5c1925df77add39de99255df
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-16 20:51:32 -0400

    meta on profile updated

--- santa/src/Santa.Logic/Objects/ProfileRecipient.cs
+++ santa/src/Santa.Logic/Objects/ProfileRecipient.cs
@@ -6,12 +6,10 @@ namespace Santa.Logic.Objects
 {
     public class ProfileRecipient
     {
-        public Guid recipientClientID { get; set; }
         //Xref ID needed for extra data for chat functionality
         public Guid? relationXrefID { get; set; }
+        public ClientMeta recipientClient { get; set; }
         public AssignmentStatus assignmentStatus { get; set; }
-        public string name { get; set; }
-        public string nickname { get; set; }
         public bool completed { get; set; }
         public Address address { get; set; }
         public Event recipientEvent { get; set; }
