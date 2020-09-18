a858e44840e23181ad6a83fdf01c06f139be9932
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-18 03:22:00 -0400

    new logic object for metas

---
+++ santa/src/Santa.Logic/Objects/AllowedAssignmentMeta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects
+{
+    public class AllowedAssignmentMeta
+    {
+        public ClientMeta clientMeta { get; set; }
+        public List<Tag> tags { get; set; }
+        public int totalSenders { get; set; }
+        public int totalAssignments { get; set; }
+    }
+}
