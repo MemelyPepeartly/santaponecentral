ae32b1a4c5721735ca4eb758f23c77660e17414f
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-20 23:03:01 -0400

    new catalogue model

---
+++ santa/src/Santa.Api/Models/Catalogue Models/searchQueryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Catalogue_Models
+{
+    public class searchQueryModel
+    {
+        public List<Guid> tags { get; set; }
+        public List<Guid> events { get; set; }
+        public List<Guid> statuses { get; set; }
+    }
+}
