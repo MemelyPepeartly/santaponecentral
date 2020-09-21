ab34b1a4ff5b68c9384077b6e7286d4b1882b99d
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-20 23:03:38 -0400

    New controller and endpoint

---
+++ santa/src/Santa.Api/Controllers/CatalogueController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Santa.Api.Models.Catalogue_Models;
+using Santa.Api.Services.Searcher_Service;
+using Santa.Data.Entities;
+using Santa.Logic.Interfaces;
+
+namespace Santa.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogueController : ControllerBase
+    {
+        private readonly ICatalogue catalogue;
+
+        public CatalogueController(ICatalogue _catalogue)
+        {
+            catalogue = _catalogue ?? throw new ArgumentNullException(nameof(_catalogue));
+        }
+
+        // POST: api/Search/Clients
+        [HttpPost]
+        public async Task<ActionResult<List<Client>>> GetClientsByQuery([FromBody] searchQueryModel model)
+        {
+            try
+            {
+                List<Logic.Objects.Client> listLogicClient = await catalogue.searchClientByQuery(model);
+
+                return Ok(listLogicClient);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
