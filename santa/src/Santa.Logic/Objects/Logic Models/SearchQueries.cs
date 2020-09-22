68782083bd60454e34dfc02fcf9579e41792bcf4
TheMemelyMan <memelypeperartly@gmail.com>
2020-09-21 22:47:20 -0400

    model updated

--- santa/src/Santa.Logic/Objects/Logic Models/SearchQueries.cs
+++ santa/src/Santa.Logic/Objects/Logic Models/SearchQueries.cs
@@ -9,5 +9,8 @@ namespace Santa.Logic.Objects
         public List<Guid> tags { get; set; }
         public List<Guid> events { get; set; }
         public List<Guid> statuses { get; set; }
+        public List<string> names { get; set; }
+        public List<string> nicknames { get; set; }
+        public bool isHardSearch { get; set; }
     }
 }
