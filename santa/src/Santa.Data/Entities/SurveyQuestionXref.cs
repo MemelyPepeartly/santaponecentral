6d25039da6401d7bcb4da3c6ffda3aeba26159e5
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-06 06:45:56 -0400

    rescaffold

--- santa/src/Santa.Data/Entities/SurveyQuestionXref.cs
+++ santa/src/Santa.Data/Entities/SurveyQuestionXref.cs
@@ -8,8 +8,8 @@ namespace Santa.Data.Entities
         public Guid SurveyQuestionXrefId { get; set; }
         public Guid SurveyId { get; set; }
         public Guid SurveyQuestionId { get; set; }
-        public string SortOrder { get; set; }
         public bool IsActive { get; set; }
+        public int SortOrder { get; set; }
 
         public virtual Survey Survey { get; set; }
         public virtual SurveyQuestion SurveyQuestion { get; set; }
