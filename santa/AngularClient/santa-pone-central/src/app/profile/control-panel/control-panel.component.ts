36c8a413afed498e765879cdd0c8fecac7ab9990
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-08 10:13:16 -0400

    Basic exporter in place

--- santa/AngularClient/santa-pone-central/src/app/profile/control-panel/control-panel.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/profile/control-panel/control-panel.component.ts
@@ -7,6 +7,7 @@ import { MapService } from 'src/app/services/mapper.service';
 import { ProfileService } from 'src/app/services/profile.service';
 import { Survey } from 'src/classes/survey';
 import { SelectedRecipientComponent } from '../selected-recipient/selected-recipient.component';
+import { AssignmentCSVModel, ExporterService } from 'src/app/services/exporter.service';
 
 @Component({
   selector: 'app-control-panel',
@@ -19,7 +20,7 @@ export class ControlPanelComponent implements OnInit {
     public SantaApiGet: SantaApiGetService,
     public auth: AuthService,
     public profileService: ProfileService,
-    public exporter: DataExportService) { }
+    public exporter: ExporterService) { }
 
   @Input() gettingAllHistories: boolean;
   @Input() gettingGeneralHistory: boolean;
@@ -88,5 +89,31 @@ export class ControlPanelComponent implements OnInit {
   public setActionTaken(event: boolean)
   {
     this.actionTaken = event;
+  }
+  public exportCSVOfAssignments()
+  {
+    let downloadModel: Array<AssignmentCSVModel> = this.convertToAssignmentCSVModel();
+
+    this.exporter.downloadFile(downloadModel, this.profile.clientNickname + "'s Assignments");
+
+
+  }
+  public convertToAssignmentCSVModel() : Array<AssignmentCSVModel>
+  {
+    let dataArray: Array<AssignmentCSVModel> = []
+    this.profile.assignments.forEach((assignment: ProfileRecipient) => {
+      dataArray.push({
+        Nickname: assignment.recipientClient.clientNickname,
+        'Real Name': assignment.recipientClient.clientName,
+        Event: assignment.recipientEvent.eventDescription,
+        'Address Line 1': assignment.address.addressLineOne,
+        'Address Line 2': assignment.address.addressLineTwo,
+        City: assignment.address.city,
+        State: assignment.address.state,
+        Country: assignment.address.country,
+        'Postal Code': assignment.address.postalCode,
+      });
+    });
+    return dataArray;
   }
 }
--- santa/AngularClient/santa-pone-central/src/app/services/exporter.service.ts
+++ santa/AngularClient/santa-pone-central/src/app/services/exporter.service.ts
@@ -1,48 +1,60 @@
 import { Injectable } from '@angular/core';
 
+export class AssignmentCSVModel
+{
+  Nickname: string;
+  'Real Name': string;
+  Event: string;
+  'Address Line 1': string;
+  'Address Line 2': string;
+  City: string;
+  State: string;
+  Country: string;
+  'Postal Code': string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ExporterService {
 
-  constructor() { }
-  static exportToCsv(filename: string, rows: object[]) {
-    if (!rows || !rows.length) {
-      return;
+  downloadFile(data, filename = 'data') {
+    let csvData = this.ConvertToCSV(data, ['Nickname', 'Real Name', 'Event', 'Address Line 1', 'Address Line 2', 'City', 'State', 'Country', 'Postal Code']);
+    console.log(csvData)
+    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
     }
-    const separator = ',';
-    const keys = Object.keys(rows[0]);
-    const csvContent =
-      keys.join(separator) +
-      '\n' +
-      rows.map(row => {
-        return keys.map(k => {
-          let cell = row[k] === null || row[k] === undefined ? '' : row[k];
-          cell = cell instanceof Date
-            ? cell.toLocaleString()
-            : cell.toString().replace(/"/g, '""');
-          if (cell.search(/("|,|\n)/g) >= 0) {
-            cell = `"${cell}"`;
-          }
-          return cell;
-        }).join(separator);
-      }).join('\n');
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", filename + ".csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
+  }
 
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-    if (navigator.msSaveBlob) { // IE 10+
-      navigator.msSaveBlob(blob, filename);
-    } else {
-      const link = document.createElement('a');
-      if (link.download !== undefined) {
-        // Browsers that support HTML5 download attribute
-        const url = URL.createObjectURL(blob);
-        link.setAttribute('href', url);
-        link.setAttribute('download', filename);
-        link.style.visibility = 'hidden';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }
+  ConvertToCSV(objArray, headerList) {
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = 'S.No,';
+
+    for (let index in headerList) {
+      row += headerList[index] + ',';
     }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = (i + 1) + '';
+      for (let index in headerList) {
+        let head = headerList[index];
+
+        line += ',' + array[i][head];
+      }
+      str += line + '\r\n';
+    }
+    return str;
   }
 }
