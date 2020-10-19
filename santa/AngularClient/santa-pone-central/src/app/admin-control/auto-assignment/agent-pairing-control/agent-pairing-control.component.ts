3251b4d96b48b13b471810c48249e3e0ab6fa70f
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-18 20:03:34 -0400

    Generated new component

---
+++ santa/AngularClient/santa-pone-central/src/app/admin-control/auto-assignment/agent-pairing-control/agent-pairing-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-pairing-control',
+  templateUrl: './agent-pairing-control.component.html',
+  styleUrls: ['./agent-pairing-control.component.css']
+})
+export class AgentPairingControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
