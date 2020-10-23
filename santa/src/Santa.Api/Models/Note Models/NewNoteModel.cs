3dd8036a44dfd9807a681be4f4bab6db5e73536d
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-23 13:15:32 -0400

    controller models made for post and put

---
+++ santa/src/Santa.Api/Models/Note Models/NewNoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Models.Note_Models
+{
+    public class NewNoteModel
+    {
+        public string noteSubject { get; set; }
+        public string noteContents { get; set; }
+    }
+}
