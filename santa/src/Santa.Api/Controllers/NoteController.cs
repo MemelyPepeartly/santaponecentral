0075c97b7dc60ea6089b51e9b600da36f5d8c5a2
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-23 17:31:30 -0400

    Authorization scheme fixed

--- santa/src/Santa.Api/Controllers/NoteController.cs
+++ santa/src/Santa.Api/Controllers/NoteController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,7 @@ namespace Santa.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class NoteController : ControllerBase
     {
         private readonly IRepository repository;
