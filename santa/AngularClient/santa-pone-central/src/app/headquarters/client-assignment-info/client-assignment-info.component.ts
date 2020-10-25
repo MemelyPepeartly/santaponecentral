4d4935058bdcd93c04d249d53821d92d0ee5cd45
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-25 15:14:22 -0400

    client assignment functional code

--- santa/AngularClient/santa-pone-central/src/app/headquarters/client-assignment-info/client-assignment-info.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/client-assignment-info/client-assignment-info.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GathererService } from 'src/app/services/gatherer.service';
+import { Client, RelationshipMeta } from 'src/classes/client';
+import { EventType } from 'src/classes/eventType';
 
 @Component({
   selector: 'app-client-assignment-info',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientAssignmentInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gatherer: GathererService) { }
 
-  ngOnInit(): void {
+  @Input() client: Client = new Client();
+
+  public events: Array<EventType> = [];
+
+  async ngOnInit() {
+    this.gatherer.gatheringAllEvents.subscribe((status: boolean) => {
+
+    });
+    this.gatherer.allEvents.subscribe((eventArray: Array<EventType>) => {
+      this.events = eventArray
+    });
+    await this.gatherer.gatherAllEvents();
   }
 
+  public getEventSenders(eventType: EventType) : Array<RelationshipMeta>
+  {
+    return this.client.senders.filter((sender: RelationshipMeta) => {return sender.eventType.eventTypeID == eventType.eventTypeID});
+  }
+  public getEventAssignments(eventType: EventType) : Array<RelationshipMeta>
+  {
+    return this.client.assignments.filter((assignment: RelationshipMeta) => {return assignment.eventType.eventTypeID == eventType.eventTypeID});
+  }
 }
