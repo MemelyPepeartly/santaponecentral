d597bcf3e2acc8ed421bb689cfc1e3a89458193a
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-29 02:48:51 -0400

    resturctured assignment meta object with new meta

--- santa/src/Santa.Logic/Objects/Information Objects/AllowedAssignmentMeta.cs
+++ santa/src/Santa.Logic/Objects/Information Objects/AllowedAssignmentMeta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Santa.Logic.Objects.Information_Objects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,7 +8,7 @@ namespace Santa.Logic.Objects
     public class AllowedAssignmentMeta
     {
         public ClientMeta clientMeta { get; set; }
-        public List<Event> clientEvents { get; set; }
+        public List<SurveyMeta> answeredSurveys { get; set; }
         public List<Tag> tags { get; set; }
         public int totalSenders { get; set; }
         public int totalAssignments { get; set; }
