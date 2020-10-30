f3014a66461ec2c334a76cb6ceeed723b3ff4c83
TheMemelyMan <memelypeperartly@gmail.com>
2020-10-30 05:56:06 -0400

    new stripped client object for searches

---
+++ santa/src/Santa.Logic/Objects/Information Objects/StrippedClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects.Information_Objects
+{
+    /// <summary>
+    /// Stripped client does not have an address, assignments, senders, or auth0 account status info
+    /// </summary>
+    public class StrippedClient
+    {
+        public Guid clientID { get; set; }
+        public Status clientStatus { get; set; }
+        public string clientName { get; set; }
+        public string nickname { get; set; }
+        public string email { get; set; }
+        public List<Response> responses { get; set; }
+        public List<Tag> tags { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
