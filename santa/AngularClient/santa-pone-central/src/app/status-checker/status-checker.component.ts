9d66e822aeae526006ee70f5b711b50a5f0079d1
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-03 15:43:12 -0500

    status checker component generated

---
+++ santa/AngularClient/santa-pone-central/src/app/status-checker/status-checker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-checker',
+  templateUrl: './status-checker.component.html',
+  styleUrls: ['./status-checker.component.css']
+})
+export class StatusCheckerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
