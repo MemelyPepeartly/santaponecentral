33f848fcca05303db5d891f4d50dd8f44c45390a
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-03 16:31:34 -0500

    functional code

--- santa/AngularClient/santa-pone-central/src/app/status-checker/status-checker.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/status-checker/status-checker.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Status } from 'src/classes/status';
+import { MapService } from '../services/mapper.service';
+import { SantaApiGetService } from '../services/santa-api.service';
 
 @Component({
   selector: 'app-status-checker',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusCheckerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public SantaApiGet: SantaApiGetService, public mapper: MapService) { }
+
+  public emailString: string = '';
+
+  public gettingStatus: boolean = false;
+  public retrievedStatus: Status = new Status();
 
   ngOnInit(): void {
   }
+  public getStatus()
+  {
+    this.gettingStatus = true;
 
+    this.SantaApiGet.getClientStatusByEmail(this.emailString).subscribe(res => {
+      this.retrievedStatus = this.mapper.mapStatus(res);
+      this.gettingStatus = false;
+    },err =>{
+      this.gettingStatus = false;
+      console.group();
+      console.log("Unable to get status");
+      console.groupEnd();
+    });
+  }
 }
