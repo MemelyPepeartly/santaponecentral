165c98a1dccf08e4f1ab04857f70b32efb21e81f
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-07 01:21:31 -0500

    new constant

--- santa/src/Santa.Logic/Constants/LoggingConstants.cs
+++ santa/src/Santa.Logic/Constants/LoggingConstants.cs
@@ -10,6 +10,6 @@ namespace Santa.Logic.Constants
         public const string GET_SPECIFIC_CLIENT_CATEGORY = "Get Client By ID";
         public const string GET_PROFILE_CATEGORY = "Get Profile";
         public const string MODIFIED_ANSWER_CATEGORY = "Modified Answer";
-
+        public const string MODIFIED_ASSIGNMENT_STATUS_CATEGORY = "Modified Assignment Status";
     }
 }
