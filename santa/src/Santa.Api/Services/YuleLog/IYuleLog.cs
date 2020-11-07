d3284e44a076789bae12f0714bc9dde070b2e68b
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-07 01:21:53 -0500

    Implimented functional logging in injectable service

--- santa/src/Santa.Api/Services/YuleLog/IYuleLog.cs
+++ santa/src/Santa.Api/Services/YuleLog/IYuleLog.cs
@@ -9,9 +9,9 @@ namespace Santa.Api.Services.YuleLog
     public interface IYuleLog
     {
         Task logGetAllClients(Client requestingClient);
-        Task logGetProfile(Client requestingClient);
-        Task logChangedAnswer(Client requestingClient);
-        Task logChangedAssignmentStatus(Client requestingClient);
-        Task logError(Client requestingClient);
+        Task logGetProfile(Client requestingClient, Profile returnedProfile);
+        Task logChangedAnswer(Client requestingClient, Question questionBeingAnsweredFor, Response oldAnswer, Response newAnswer);
+        Task logChangedAssignmentStatus(Client requestingClient, string assignmentNickname, AssignmentStatus oldStatus, AssignmentStatus newStatus);
+        Task logError(Client requestingClient, string category);
     }
 }
--- santa/src/Santa.Api/Services/YuleLog/YuleLog.cs
+++ santa/src/Santa.Api/Services/YuleLog/YuleLog.cs
@@ -1,4 +1,6 @@
-﻿using Santa.Logic.Constants;
+﻿using Santa.Api.Models.Assignment_Status_Models;
+using Santa.Data.Entities;
+using Santa.Logic.Constants;
 using Santa.Logic.Interfaces;
 using Santa.Logic.Objects;
 using Santa.Logic.Objects.Base_Objects.Logging;
@@ -6,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AssignmentStatus = Santa.Logic.Objects.AssignmentStatus;
+using Category = Santa.Logic.Objects.Base_Objects.Logging.Category;
+using Client = Santa.Logic.Objects.Client;
 
 namespace Santa.Api.Services.YuleLog
 {
@@ -18,29 +23,53 @@ namespace Santa.Api.Services.YuleLog
             repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
         }
 
-        public async Task logChangedAnswer(Client requestingClient)
+        public async Task logChangedAnswer(Client requestingClient, Question questionBeingAnsweredFor, Response oldAnswer, Response newAnswer)
         {
-            throw new NotImplementedException();
+            string logMessage = $@"{requestingClient.nickname} made a request to change their answer for question '{questionBeingAnsweredFor.questionText}' from '{oldAnswer.responseText}' to '{newAnswer.responseText}'";
+            await repository.CreateNewLogEntry(makeLogTemplateObject(await getCategoryByName(LoggingConstants.MODIFIED_ANSWER_CATEGORY), logMessage));
         }
 
-        public async Task logChangedAssignmentStatus(Client requestingClient)
+        public async Task logChangedAssignmentStatus(Client requestingClient, string assignmentNickname, AssignmentStatus oldStatus, AssignmentStatus newStatus)
         {
-            throw new NotImplementedException();
+            string logMessage = $@"{requestingClient.nickname} made a request to change the assignment status for {assignmentNickname} from '{oldStatus.assignmentStatusName}' to '{newStatus.assignmentStatusName}'";
+            await repository.CreateNewLogEntry(makeLogTemplateObject(await getCategoryByName(LoggingConstants.MODIFIED_ASSIGNMENT_STATUS_CATEGORY), logMessage));
         }
 
-        public async Task logError(Client requestingClient)
+        public async Task logError(Client requestingClient, string category)
         {
-            throw new NotImplementedException();
+            Category errorCategory = await getCategoryByName(LoggingConstants.MODIFIED_ANSWER_CATEGORY);
+            string logMessage = "";
+            if(errorCategory.categoryName == LoggingConstants.GET_ALL_CLIENT_CATEGORY)
+            {
+                logMessage = "There was an error getting all clients";
+            }
+            if(errorCategory.categoryName == LoggingConstants.GET_PROFILE_CATEGORY)
+            {
+                logMessage = "There was an error retrieving a profile";
+            }
+            if (errorCategory.categoryName == LoggingConstants.GET_SPECIFIC_CLIENT_CATEGORY)
+            {
+                logMessage = "There was an error getting the client data";
+            }
+            if (errorCategory.categoryName == LoggingConstants.MODIFIED_ANSWER_CATEGORY)
+            {
+                logMessage = "There was an error changing the answer";
+            }
+            if (errorCategory.categoryName == LoggingConstants.MODIFIED_ASSIGNMENT_STATUS_CATEGORY)
+            {
+                logMessage = "There was an error modifying the assignment's status";
+            }
+            await repository.CreateNewLogEntry(makeLogTemplateObject(errorCategory, logMessage));
         }
 
         public async Task logGetAllClients(Client requestingClient)
         {
-            throw new NotImplementedException();
+            await repository.CreateNewLogEntry(makeLogTemplateObject(await getCategoryByName(LoggingConstants.GET_ALL_CLIENT_CATEGORY), requestingClient.nickname + " made a request to retrieve a list of all clients"));
         }
 
-        public async Task logGetProfile(Client requestingClient)
+        public async Task logGetProfile(Client requestingClient, Profile returnedProfile)
         {
-            throw new NotImplementedException();
+            await repository.CreateNewLogEntry(makeLogTemplateObject(await getCategoryByName(LoggingConstants.GET_PROFILE_CATEGORY), requestingClient.nickname + " made a request to get a profile for: " + returnedProfile.nickname));
         }
         private Logic.Objects.Base_Objects.Logging.YuleLog makeLogTemplateObject(Category logicCategory, string logMessage)
         {
