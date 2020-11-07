64cf28bd0c3b61c5d2b63ee620b79a761efb9ec5
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-06 22:51:48 -0500

    DB rescaffold

---
+++ santa/src/Santa.Data/Entities/YuleLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Santa.Data.Entities
+{
+    public partial class YuleLog
+    {
+        public Guid LogId { get; set; }
+        public DateTime LogDate { get; set; }
+        public Guid Category { get; set; }
+        public string LogText { get; set; }
+
+        public virtual Category CategoryNavigation { get; set; }
+    }
+}
