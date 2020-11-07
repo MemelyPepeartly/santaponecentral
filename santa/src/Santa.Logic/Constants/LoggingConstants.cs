617bda34ab3db2b74e7524930dddd5422eb5939b
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-07 07:01:51 -0500

    New constants added

--- santa/src/Santa.Logic/Constants/LoggingConstants.cs
+++ santa/src/Santa.Logic/Constants/LoggingConstants.cs
@@ -6,9 +6,16 @@ namespace Santa.Logic.Constants
 {
     public static class LoggingConstants
     {
+        public const string POSTED_ASSIGNMENT_CATEGORY = "Assignments Given";
+
         public const string GET_ALL_CLIENT_CATEGORY = "Get All Clients";
         public const string GET_SPECIFIC_CLIENT_CATEGORY = "Get Client By ID";
         public const string GET_PROFILE_CATEGORY = "Get Profile";
+        public const string GET_SPECIFIC_HISTORY_CATEGORY = "Get History";
+        public const string GET_ALL_HISTORY_CATEGORY = "Get All History";
+
+        public const string MODIFIED_CLIENT_CATEGORY = "Modified Client";
+        public const string MODIFIED_PROFILE_CATEGORY = "Modified Profile";
         public const string MODIFIED_ANSWER_CATEGORY = "Modified Answer";
         public const string MODIFIED_ASSIGNMENT_STATUS_CATEGORY = "Modified Assignment Status";
     }
