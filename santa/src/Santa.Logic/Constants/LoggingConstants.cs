8094a0e737e397a8faf46a055ac2438925a64658
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-07 00:56:10 -0500

    logging constant object

---
+++ santa/src/Santa.Logic/Constants/LoggingConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Constants
+{
+    public static class LoggingConstants
+    {
+        public const string GET_ALL_CLIENT_CATEGORY = "Get All Clients";
+        public const string GET_SPECIFIC_CLIENT_CATEGORY = "Get Client By ID";
+        public const string GET_PROFILE_CATEGORY = "Get Profile";
+        public const string MODIFIED_ANSWER_CATEGORY = "Modified Answer";
+
+    }
+}
