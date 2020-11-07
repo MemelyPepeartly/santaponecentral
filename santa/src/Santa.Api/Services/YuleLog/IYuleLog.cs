cc1fb293b806cba449c677acaec67ac63ec45fa9
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-07 00:25:25 -0500

    new YuleLog service bones in place

---
+++ santa/src/Santa.Api/Services/YuleLog/IYuleLog.cs
@@ -0,0 +1,17 @@
+﻿using Santa.Logic.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Services.YuleLog
+{
+    public interface IYuleLog
+    {
+        Task logGetAllClients(Client requestingClient);
+        Task logGetProfile(Client requestingClient);
+        Task logChangedAnswer(Client requestingClient);
+        Task logChangedAssignmentStatus(Client requestingClient);
+        Task logError(Client requestingClient);
+    }
+}
---
+++ santa/src/Santa.Api/Services/YuleLog/YuleLog.cs
@@ -0,0 +1,44 @@
+﻿using Santa.Logic.Interfaces;
+using Santa.Logic.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Santa.Api.Services.YuleLog
+{
+    public class YuleLog : IYuleLog
+    {
+        private readonly IRepository repository;
+
+        public YuleLog(IRepository _repository)
+        {
+            repository = _repository ?? throw new ArgumentNullException(nameof(_repository));
+        }
+
+        public Task logChangedAnswer(Client requestingClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task logChangedAssignmentStatus(Client requestingClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task logError(Client requestingClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task logGetAllClients(Client requestingClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task logGetProfile(Client requestingClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
