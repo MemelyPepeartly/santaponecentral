5070d3a437752508da019a73a329ffcb3ab6d2cf
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-09 17:51:53 -0500

    new info container note functionality

--- santa/AngularClient/santa-pone-central/src/app/headquarters/client-note-info/client-note-info.component.ts
+++ santa/AngularClient/santa-pone-central/src/app/headquarters/client-note-info/client-note-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Client } from 'src/classes/client';
+import { MapService } from 'src/app/services/mapper.service';
+import { SantaApiGetService } from 'src/app/services/santa-api.service';
+import { Client, HQClient } from 'src/classes/client';
 
 @Component({
   selector: 'app-client-note-info',
@@ -8,14 +10,19 @@ import { Client } from 'src/classes/client';
 })
 export class ClientNoteInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public SantaApiGet: SantaApiGetService, public mapper: MapService) { }
 
-  @Input() client: Client = new Client();
+  @Input() client: HQClient = new HQClient();
 
   @Output() refreshEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() clickawayLockedEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  public gatheringInfoContainer: boolean = false;
+
+  async ngOnInit() {
+    this.gatheringInfoContainer = true;
+    this.client.infoContainer = this.mapper.mapInfoContainer(await this.SantaApiGet.getInfoContainerByClientID(this.client.clientID).toPromise());
+    this.gatheringInfoContainer = false;
   }
   public emitRefreshAction()
   {
