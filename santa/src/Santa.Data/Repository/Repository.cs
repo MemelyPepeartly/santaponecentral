34210fdca0f1c0f9b7ece396438a39912169fdc0
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-09 17:49:33 -0500

    new repo funtionality for HQ clients

--- santa/src/Santa.Data/Repository/Repository.cs
+++ santa/src/Santa.Data/Repository/Repository.cs
@@ -87,6 +87,30 @@ namespace Santa.Data.Repository
             }).ToListAsync();
             return clientList;
         }
+        public async Task<List<HQClient>> GetAllHeadquarterClients()
+        {
+            List<HQClient> clientList = await santaContext.Client
+                .Select(client => new HQClient()
+                {
+                    clientID = client.ClientId,
+                    email = client.Email,
+                    nickname = client.Nickname,
+                    clientName = client.ClientName,
+                    isAdmin = client.IsAdmin,
+                    hasAccount = client.HasAccount,
+                    clientStatus = Mapper.MapStatus(client.ClientStatus),
+                    answeredSurveys = client.SurveyResponse.Select(r => r.Survey.SurveyId).Distinct().ToList(),
+                    assignments = client.ClientRelationXrefSenderClient.Count(),
+                    senders = client.ClientRelationXrefRecipientClient.Count(),
+                    tags = client.ClientTagXref.Select(tagXref => new Logic.Objects.Tag()
+                    {
+                        tagID = tagXref.TagId,
+                        tagName = tagXref.Tag.TagName,
+                    }).ToList(),
+
+                }).AsNoTracking().ToListAsync();
+            return clientList;
+        }
         public async Task<List<Logic.Objects.Client>> GetAllClients()
         {
             List<Logic.Objects.Client> clientList = await santaContext.Client
@@ -1354,14 +1378,47 @@ namespace Santa.Data.Repository
         #endregion
 
         #region Informational
-        public async Task<List<RelationshipMeta>> getClientAssignmentsByIDAsync(Guid clientID)
+        public async Task<InfoContainer> getClientInfoContainerByIDAsync(Guid clientID)
         {
-            throw new NotImplementedException();
-        }
+            InfoContainer logicRelationshipContainer = await santaContext.Client.Select(client => new InfoContainer()
+            {
+                agentID = client.ClientId,
+                notes = client.Note.Select(note => new Logic.Objects.Base_Objects.Note()
+                {
+                    noteID = note.NoteId,
+                    noteSubject = note.NoteSubject,
+                    noteContents = note.NoteContents
+                }).ToList(),
+                assignments = client.ClientRelationXrefSenderClient.Select(xref => new RelationshipMeta()
+                {
+                    relationshipClient = Mapper.MapClientMeta(xref.RecipientClient),
+                    eventType = Mapper.MapEvent(xref.EventType),
+                    clientRelationXrefID = xref.ClientRelationXrefId,
+                    assignmentStatus = Mapper.MapAssignmentStatus(xref.AssignmentStatus),
+                    tags = xref.RecipientClient.ClientTagXref.ToList().Select(tagXref => new Logic.Objects.Tag()
+                    {
+                        tagID = tagXref.TagId,
+                        tagName = tagXref.Tag.TagName
+                    }).OrderBy(t => t.tagName).ToList(),
 
-        public async Task<List<RelationshipMeta>> getClientSendersByIDAsync(Guid clientID)
-        {
-            throw new NotImplementedException();
+                    removable = xref.ChatMessage.Count > 0 ? false : true
+                }).ToList(),
+                senders = client.ClientRelationXrefRecipientClient.Select(xref => new RelationshipMeta()
+                {
+                    relationshipClient = Mapper.MapClientMeta(xref.SenderClient),
+                    eventType = Mapper.MapEvent(xref.EventType),
+                    clientRelationXrefID = xref.ClientRelationXrefId,
+                    assignmentStatus = Mapper.MapAssignmentStatus(xref.AssignmentStatus),
+                    tags = xref.SenderClient.ClientTagXref.ToList().Select(tagXref => new Logic.Objects.Tag()
+                    {
+                        tagID = tagXref.TagId,
+                        tagName = tagXref.Tag.TagName
+                    }).OrderBy(t => t.tagName).ToList(),
+
+                    removable = xref.ChatMessage.Count > 0 ? false : true
+                }).ToList()
+            }).FirstOrDefaultAsync(c => c.agentID == clientID);
+            return logicRelationshipContainer;
         }
         #endregion
 
---
+++ santa/src/Santa.Logic/Objects/Information Objects/HQClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects.Information_Objects
+{
+    public class HQClient
+    {
+        public Guid clientID { get; set; }
+        public Status clientStatus { get; set; }
+        public string clientName { get; set; }
+        public string nickname { get; set; }
+        public string email { get; set; }
+        public InfoContainer infoContainer { get; set; }
+        public List<Guid> answeredSurveys { get; set; }
+        public int assignments { get; set; }
+        public int senders { get; set; }
+        public List<Tag> tags { get; set; }
+        public bool isAdmin { get; set; }
+        /// <summary>
+        /// Determines if they have an auth0 account. This is false if they are a manual signup by default
+        /// </summary>
+        public bool hasAccount { get; set; }
+    }
+}
