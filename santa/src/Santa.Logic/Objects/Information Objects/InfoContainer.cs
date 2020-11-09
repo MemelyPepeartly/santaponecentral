ba7e6c76e73391baa879d530eb04fd75ebf26722
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-09 17:49:12 -0500

    new info container

---
+++ santa/src/Santa.Logic/Objects/Information Objects/InfoContainer.cs
@@ -0,0 +1,15 @@
+﻿using Santa.Logic.Objects.Base_Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Santa.Logic.Objects.Information_Objects
+{
+    public class InfoContainer
+    {
+        public Guid agentID { get; set; }
+        public List<RelationshipMeta> senders { get; set; }
+        public List<RelationshipMeta> assignments { get; set; }
+        public List<Note> notes { get; set; }
+    }
+}
