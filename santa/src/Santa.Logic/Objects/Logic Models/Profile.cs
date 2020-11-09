66ceca16732cca784953fd662792335a78286802
TheMemelyMan <memelypeperartly@gmail.com>
2020-11-09 00:08:07 -0500

    mapper fixes

--- santa/src/Santa.Logic/Objects/Logic Models/Profile.cs
+++ santa/src/Santa.Logic/Objects/Logic Models/Profile.cs
@@ -7,7 +7,6 @@ namespace Santa.Logic.Objects
     public class Profile
     {
         public Guid clientID { get; set; }
-        public Status clientStatus { get; set; }
         public string clientName { get; set; }
         public string nickname { get; set; }
         public string email { get; set; }
