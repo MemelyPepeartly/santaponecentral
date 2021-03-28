466bd2c659390aeb1eefbb2aadad46db3adec75c
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-03-28 16:47:45 -0600

    Logic models in place

---
+++ SPC-2021/api/survey/survey/Survey.Logic/Objects/Option.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Survey.Logic.Objects
+{
+    public class Option
+    {
+        public Guid surveyOptionID { get; set; }
+        public string displayText { get; set; }
+        public int sortOrder { get; set; }
+        public string surveyOptionValue { get; set; }
+        public bool removable { get; set; }
+    }
+}
