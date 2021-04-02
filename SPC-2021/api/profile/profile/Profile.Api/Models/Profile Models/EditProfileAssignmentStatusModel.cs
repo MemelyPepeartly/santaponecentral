4d1c568f01f97a2c1c51ff6ed9a2b45ddf933eb6
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-02 00:55:55 -0600

    Profile API proj merged

---
+++ SPC-2021/api/profile/profile/Profile.Api/Models/Profile Models/EditProfileAssignmentStatusModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profile.Api.Models.Profile_Models
+{
+    public class EditProfileAssignmentStatusModel
+    {
+        public Guid assignmentStatusID { get; set; }
+    }
+}
--- SPC-2021/api/profile/profile/Profile.Api/Startup.cs
+++ SPC-2021/api/profile/profile/Profile.Api/Startup.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Profile.Data.Entities;
+using Profile.Data.Repository;
+using Profile.Logic.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +21,7 @@ namespace Profile.Api
 {
     public class Startup
     {
+        private const string version = "v1";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,12 +32,78 @@ namespace Profile.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connection string
+            string connectionString = Configuration.GetConnectionString("SantaBaseAppDb");
 
-            services.AddControllers();
+            //DBContext
+            services.AddDbContext<SantaPoneCentralDatabaseContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            }, ServiceLifetime.Transient);
+
+            //Cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngular",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200", "https://dev-santaponecentral.azurewebsites.net", "https://www.santaponecentral.net", "https://santaponecentral.azurewebsites.net")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+
+            //Services
+            services.AddScoped<IRepository, Repository>();
+
+            //Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Profile.Api", Version = "v1" });
+                c.SwaggerDoc(version, new OpenApiInfo { Title = "SantaPone API", Version = version });
+                c.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    Description = "Bearer authentication scheme with JWT, e.g. \"Bearer eyJhbGciOiJIUzI1NiJ9.e30\"",
+                    Name = "Authorization"
+                });
+                c.OrderActionsBy((apiDesc) => $"{apiDesc.ActionDescriptor.RouteValues["controller"]}_{apiDesc.HttpMethod}");
             });
+
+            //SignalR
+            services.AddSignalR();
+
+            //Auth
+            string domain = $"https://{Configuration["Auth0API:domain"]}/";
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0API:startupAudience"];
+            });
+
+            services.AddAuthorization(options =>
+            {
+
+                var objects = new[] { "clients", "events", "statuses", "surveys", "responses", "tags", "profile", "messages", "histories", "boardEntries", "entryTypes", "assignmentStatuses", "logs", "categories" };
+                var verbs = new[] { "create", "read", "update", "delete" };
+
+                // cartesian product
+                var permissions = objects.SelectMany(o => verbs.Select(v => $"{v}:{o}"));
+                foreach (string permission in permissions)
+                {
+                    options.AddPolicy(permission, policy => policy.RequireClaim("permissions", permission));
+                }
+
+            });
+
+            services.AddMvc();
+
+            services.AddControllers();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,16 +112,24 @@ namespace Profile.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Profile.Api v1"));
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
+            //Auth
             app.UseAuthorization();
+            app.UseAuthentication();
 
+            app.UseCors("AllowAngular");
+
+            //Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/" + version + "/swagger.json", "SantaPone Central " + version.ToUpper());
+            });
+
+            //Endpoints
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
