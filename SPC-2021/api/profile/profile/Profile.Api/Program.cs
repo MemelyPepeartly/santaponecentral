23a43b33a0b8990c855dc3618160719e05e2f5c2
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-01 23:18:22 -0600

    Basic profile project gen and setup

--- SPC-2021/api/profile/profile/Profile.Api/Program.cs
+++ SPC-2021/api/profile/profile/Profile.Api/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace profile
+namespace Profile.Api
 {
     public class Program
     {
--- SPC-2021/api/profile/profile/Profile.Api/Startup.cs
+++ SPC-2021/api/profile/profile/Profile.Api/Startup.cs
@@ -6,12 +6,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace profile
+namespace Profile.Api
 {
     public class Startup
     {
@@ -25,7 +26,12 @@ namespace profile
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Profile.Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +40,8 @@ namespace profile
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Profile.Api v1"));
             }
 
             app.UseHttpsRedirection();
