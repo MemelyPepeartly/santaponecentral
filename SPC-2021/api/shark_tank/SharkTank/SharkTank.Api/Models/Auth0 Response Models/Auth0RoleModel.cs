a68ad065228b9cd74db3d1ebeaa1167034220997
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-06 01:48:53 -0600

    models added

---
+++ SPC-2021/api/shark_tank/SharkTank/SharkTank.Api/Models/Auth0 Response Models/Auth0RoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharkTank.Api.Models.Auth0_Response_Models
+{
+    public class Auth0RoleModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
