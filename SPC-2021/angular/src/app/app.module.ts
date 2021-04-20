daadeb4d6e4bf0896b65a9611f76f2bbea766d60
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-19 18:51:22 -0600

    ng add angular material

--- SPC-2021/angular/src/app/app.module.ts
+++ SPC-2021/angular/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './core/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- SPC-2021/angular/src/app/services/countries.service.ts
+++ SPC-2021/angular/src/app/services/countries.service.ts
@@ -1,8 +1,5 @@
   
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable} from 'rxjs';
-import { map } from 'rxjs/operators';
 import * as countries from "../../assets/data/countries.json"
 
 @Injectable({
