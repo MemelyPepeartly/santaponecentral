15825a85a1c2f3f3613d371e54fa923dcd636ab6
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-19 19:14:37 -0600

    shared components imported

--- SPC-2021/angular/src/app/app.module.ts
+++ SPC-2021/angular/src/app/app.module.ts
@@ -6,9 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomMaterialModule } from './core/material.module';
 
+import { ChatHistoriesComponent } from './shared/chat-histories/chat-histories.component';
+import { AssignmentStatusControllerComponent } from './shared/assignment-status-controller/assignment-status-controller.component';
+import { ContactPanelComponent } from './shared/contact-panel/contact-panel.component';
+import { InputControlComponent } from './shared/input-control/input-control.component';
+import { LoadingSpinComponent } from './shared/loading-spin/loading-spin.component';
+import { SmallLoadingSpinComponent } from './shared/small-loading-spin/small-loading-spin.component';
+import { ResponseListComponent } from './shared/response-list/response-list.component';
+import { MissionBoardTableComponent } from './shared/mission-board-table/mission-board-table.component';
+import { MessageControlPanelComponent } from './shared/message-control-panel/message-control-panel.component';
+import { ChatComponent } from './shared/chat/chat.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatHistoriesComponent,
+    AssignmentStatusControllerComponent,
+    ContactPanelComponent,
+    InputControlComponent,
+    LoadingSpinComponent,
+    SmallLoadingSpinComponent,
+    ResponseListComponent,
+    MissionBoardTableComponent,
+    MessageControlPanelComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
---
+++ SPC-2021/angular/src/app/shared/constants/organizerEmailConstants.enum.ts
@@ -0,0 +1,5 @@
+export enum OrganizerEmailConstants {
+  SANTAPONE = "mlpsantapone@gmail.com",
+  CARDDRAGON = "thecardslut@gmail.com",
+  DEV = "santaponecentraldev@gmail.com"
+}
