5d361c526133a13927778b604265ccbd8ca6e1fe
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-23 18:03:43 -0600

    new auth request model

---
+++ SPC-2021/api/shark-tank/shark-tank/SharkTank.Logic/Models/Auth0 Response Models/Auth0InfoRequestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTank.Logic.Models.Auth0_Response_Models
+{
+    public class Auth0InfoRequestModel
+    {
+        /// <summary>
+        /// The ID of the client making the request for information
+        /// </summary>
+        public Guid? requestingClientID { get; set; }
+        /// <summary>
+        /// The email of the client making the request for information
+        /// </summary>
+        public string? requestingClientEmail { get; set; }
+        /// <summary>
+        /// Requested clientID of the information the requester wants to access
+        /// </summary>
+        public Guid? requestedClientID { get; set; }
+        /// <summary>
+        /// Requested email of the information the requester wants to access
+        /// </summary>
+        public string? requestedClientEmail { get; set; }
+    }
+}
