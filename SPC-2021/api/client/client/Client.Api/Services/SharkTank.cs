bcd513a2d039a64eca35290b80bec1241adc16ac
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-04-30 14:44:51 -0600

    Implimented interface

--- SPC-2021/api/client/client/Client.Api/Services/SharkTank.cs
+++ SPC-2021/api/client/client/Client.Api/Services/SharkTank.cs
@@ -1,4 +1,5 @@
 ﻿using Client.Logic.Interfaces;
+using Client.Logic.Models.Auth0_Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,39 @@ namespace Client.Api.Services
 {
     public class SharkTank : ISharkTank
     {
+        public async Task<object> CheckIfValidRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> DeleteAuthUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Auth0UserInfoModel> GetAuthInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> PostNewAuthUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> PutEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> PutName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<object> PutPasswordTicketRequest()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
