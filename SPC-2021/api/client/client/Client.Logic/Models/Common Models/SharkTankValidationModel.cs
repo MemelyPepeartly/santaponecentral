829f4c213c091c11a320748be72cb689b0836853
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-04 23:13:01 -0500

    added common model to client

---
+++ SPC-2021/api/client/client/Client.Logic/Models/Common Models/SharkTankValidationModel.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Logic.Models.Common_Models
+{
+    public class SharkTankValidationModel
+    {
+        public Guid? requestorClientID { get; set; }
+        public Guid? requestObject { get; set; }
+        public List<string> requestorRoles { get; set; }
+        public Method httpMethod { get; set; }
+    }
+}
