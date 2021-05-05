66044640d54ab48eb9588119b634cf3a9e2266d3
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-05 01:02:50 -0600

    New status model

---
+++ SPC-2021/api/client/client/Client.Logic/Models/Status Models/ApiStatusDescription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Logic.Models.Status_Models
+{
+    public class ApiStatusDescription
+    {
+        public string statusDescription { get; set; }
+    }
+}
