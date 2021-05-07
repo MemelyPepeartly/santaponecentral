35a35caf0f296c33b403f344f9d9742211491ff5
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-07 02:15:15 -0600

    environments updated with service endpoints

--- SPC-2021/angular/src/environments/environment.dev.ts
+++ SPC-2021/angular/src/environments/environment.dev.ts
@@ -4,9 +4,16 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://dev-santaponecentral-api.azurewebsites.net/api/",
   auth0Domain: "memelydev.auth0.com",
   auth0Client_id: "KvZyPvtRblUBt2clTAmJx84RT4mwmZ3L",
   auth0Redirect_uri: `${window.location.origin}`,
-  auth0Audience: "https://dev-santaponecentral-api.azurewebsites.net/api/"
+  auth0Audience: "https://dev-santaponecentral-api.azurewebsites.net/api/",
+  // API ENDPOINTS
+  clientServiceEndpoint: "https://dev-spc-clientapi.azurewebsites.net",
+  eventServiceEndpoint: "https://dev-spc-eventapi.azurewebsites.net",
+  messageServiceEndpoint: "https://dev-spc-messageapi.azurewebsites.net",
+  profileServiceEndpoint: "https://dev-spc-profileapi.azurewebsites.net",
+  searchServiceEndpoint: "https://dev-spc-searchapi.azurewebsites.net",
+  sharkTankServiceEndpoint: "https://dev-spc-sharktankapi.azurewebsites.net",
+  surveyServiceEndpoint: "https://dev-spc-surveyapi.azurewebsites.net"
 };
--- SPC-2021/angular/src/environments/environment.prod.ts
+++ SPC-2021/angular/src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
 export const environment = {
   production: true,
-  apiUrl: "https://santaponecentral-api.azurewebsites.net/api/",
   auth0Domain: "santaponecentral.us.auth0.com",
   auth0Client_id: "U1PyIC5MkHe4fy8Rf9V0vDUQRlnbA8NS",
   auth0Redirect_uri: `${window.location.origin}`,
-  auth0Audience: "https://santaponecentral-api.azurewebsites.net/"
+  auth0Audience: "https://santaponecentral-api.azurewebsites.net/",
+  // API ENDPOINTS
+  clientServiceEndpoint: "",
+  eventServiceEndpoint: "",
+  messageServiceEndpoint: "",
+  profileServiceEndpoint: "",
+  searchServiceEndpoint: "",
+  sharkTankServiceEndpoint: "",
+  surveyServiceEndpoint: ""
 };
