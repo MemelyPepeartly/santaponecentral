6f1ccb3668985c4e37ea6bfa6b6b4f3cd7e6f190
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-07 02:29:17 -0600

    client and status methods in place

--- SPC-2021/angular/src/app/services/api services/client.service.ts
+++ SPC-2021/angular/src/app/services/api services/client.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { StrippedClient } from 'src/classes/client';
+import { BaseClient, Client, HQClient, InfoContainer, StrippedClient } from 'src/classes/client';
+import { Status } from 'src/classes/status';
 
 const endpoint = environment.clientServiceEndpoint;
 
@@ -19,8 +20,56 @@ export class ClientService
 {
   constructor(private http: HttpClient) { }
 
-  getAllTruncatedClients(): Observable<Array<StrippedClient>>
+  getAllTruncatedClients(): Observable<Array<StrippedClient>> 
   {
     return this.http.get<Array<StrippedClient>>(endpoint + 'Client/Truncated')
+  }
+  getTruncatedClientByID(id: string): Observable<StrippedClient> 
+  {
+    return this.http.get<StrippedClient>(endpoint + 'Client/Truncated/' + id)
+  }
+  getAllHQClients(): Observable<Array<HQClient>> 
+  {
+    return this.http.get<Array<HQClient>>(endpoint + 'Client/HQClient')
+  }
+  getHQClientByID(id: string): Observable<HQClient> 
+  {
+    return this.http.get<HQClient>(endpoint + 'Client/HQClient/' + id)
+  }
+  getInfoContainerByClientID(id: string): Observable<InfoContainer> 
+  {
+    return this.http.get<InfoContainer>(endpoint + 'Client/' + id + "/InfoContainer")
+  }
+  getClientByClientID(id: string): Observable<Client> 
+  {
+    return this.http.get<Client>(endpoint + 'Client/' + id)
+  }
+  getClientByEmail(email: string): Observable<Client>  
+  {
+    return this.http.get<Client>(endpoint + 'Client/Email/' + email)
+  }
+  getBasicClientByClientID(id: string): Observable<BaseClient> 
+  {
+    return this.http.get<BaseClient>(endpoint + 'Client/Basic/' + id)
+  }
+  getBasicClientByEmail(email: string): Observable<BaseClient>
+  {
+    return this.http.get<BaseClient>(endpoint + 'Client/Basic/Email/' + email)
+  }
+  getAllowedAssignmentsByID(clientID: string, eventTypeID: string) : Observable<any> 
+  {
+    return this.http.get(endpoint + 'Client/' + clientID + '/AllowedAssignment/' + eventTypeID)
+  }
+  getAllStatuses(): Observable<Array<Status>> 
+  {
+    return this.http.get<Array<Status>>(endpoint + 'Status')
+  }
+  getStatus(id): Observable<Status> 
+  {
+    return this.http.get<Status>(endpoint + 'Status/' + id)
+  }
+  getClientStatusByEmail(email: string): Observable<Status> 
+  {
+    return this.http.get<Status>(endpoint + 'Status/Check/' + email)
   }
 }
