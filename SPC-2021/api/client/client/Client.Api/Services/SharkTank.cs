2328be8c842db96675d3dbf435b423bf376458a6
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-07 01:16:14 -0600

    Sharktank arg update

--- SPC-2021/api/client/client/Client.Api/Services/SharkTank.cs
+++ SPC-2021/api/client/client/Client.Api/Services/SharkTank.cs
@@ -34,10 +34,11 @@ namespace Client.Api.Services
             return JsonConvert.DeserializeObject<SharkTankValidationResponseModel>(response.Content);
         }
 
-        public async Task<object> DeleteAuthUser(Guid clientID)
+        public async Task<object> DeleteAuthUser(string authClientID)
         {
+#warning double check this endpoint to see if it is better to use authclientID or normal clientID
             // Setup rest client
-            RestClient userRestClient = new RestClient($"{endpoint}SharkTank/{clientID}");
+            RestClient userRestClient = new RestClient($"{endpoint}SharkTank/{authClientID}");
             RestRequest userRequest = new RestRequest(Method.DELETE);
             // New request object for body
             object request = new object();
@@ -47,7 +48,7 @@ namespace Client.Api.Services
             return response;
         }
 
-        public async Task<Auth0UserInfoModel> GetAuthInfo()
+        public async Task<Auth0UserInfoModel> GetAuthInfo(string authEmail)
         {
             // Setup rest client
             RestClient userRestClient = new RestClient(endpoint + "SharkTank/AuthInfo");
@@ -60,7 +61,7 @@ namespace Client.Api.Services
             return JsonConvert.DeserializeObject<Auth0UserInfoModel>(response.Content);
         }
 
-        public async Task<object> MakeNewFailiureLog(BaseClient requestingClient, string category)
+        public async Task<object> MakeNewFailureLog(BaseClient requestingClient, string category)
         {
             // Setup rest client
             RestClient userRestClient = new RestClient($"{endpoint}SharkTank/NewLog");
--- SPC-2021/api/client/client/Client.Logic/Interfaces/ISharkTank.cs
+++ SPC-2021/api/client/client/Client.Logic/Interfaces/ISharkTank.cs
@@ -8,14 +8,14 @@ namespace Client.Logic.Interfaces
 {
     public interface ISharkTank
     {
-        Task<Auth0UserInfoModel> GetAuthInfo();
+        Task<Auth0UserInfoModel> GetAuthInfo(string authEmail);
         Task<object> PostNewAuthUser();
         Task<SharkTankValidationResponseModel> CheckIfValidRequest(SharkTankValidationModel requestModel);
         Task<object> PutAuthEmail(Guid clientID);
         Task<object> PutAuthName(Guid clientID);
         Task<object> PutPasswordTicketRequest(Guid clientID);
-        Task<object> DeleteAuthUser(Guid clientID);
+        Task<object> DeleteAuthUser(string authClientID);
         Task<object> MakeNewSuccessLog(BaseClient requestingClient, string category);
-        Task<object> MakeNewFailiureLog(BaseClient requestingClient, string category);
+        Task<object> MakeNewFailureLog(BaseClient requestingClient, string category);
     }
 }
