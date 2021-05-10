6290879191a812e74bd72689c3074c8339183611
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-09 19:52:45 -0600

    Shark tank methods

--- SPC-2021/angular/src/app/services/api services/shark-tank.service.ts
+++ SPC-2021/angular/src/app/services/api services/shark-tank.service.ts
@@ -18,6 +18,7 @@ export class SharkTankService
 {
   constructor(private http: HttpClient) { }
 
+  /* GET */
   getAllCategories(): Observable<any> 
   {
     return this.http.get(endpoint + 'Category');
@@ -29,5 +30,15 @@ export class SharkTankService
   getAllLogsByCategoryID(id: string): Observable<any> 
   {
     return this.http.get(endpoint + 'Log/Category/' + id);
+  }
+
+  /* POST */
+  postPasswordResetToClient(id: string): Observable<any> 
+  {
+    return this.http.post(endpoint + 'Client/' + id + "/Password", {});
+  }
+  postClientNewAuth0Account(clientID: string): Observable<any> 
+  {
+    return this.http.post(endpoint + 'Client/' + clientID + '/CreateAccount', {});
   }
 }
