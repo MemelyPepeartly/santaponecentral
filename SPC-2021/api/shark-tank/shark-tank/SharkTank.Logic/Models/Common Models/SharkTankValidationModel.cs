8eabadf6212e831c3733bfe75df6214b1ee4e37f
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-23 03:12:24 -0600

    ported over models

---
+++ SPC-2021/api/shark-tank/shark-tank/SharkTank.Logic/Models/Common Models/SharkTankValidationModel.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTank.Logic.Models.Common_Models
+{
+    public class SharkTankValidationModel
+    {
+        public Guid requestorClientID { get; set; }
+        public string requesetedObject { get; set; }
+        public List<Claim> requestorRoles { get; set; }
+        public Method httpMethod { get; set; }
+    }
+}
