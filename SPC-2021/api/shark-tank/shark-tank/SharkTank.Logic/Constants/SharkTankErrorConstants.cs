92899382b8fc70633cfb68697bafe79222a4f072
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-29 01:20:00 -0600

    new error constant

---
+++ SPC-2021/api/shark-tank/shark-tank/SharkTank.Logic/Constants/SharkTankErrorConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkTank.Logic.Constants
+{
+    public static class SharkTankErrorConstants
+    {
+        public const string INVALID_REQUEST_ERROR = "There was something wrong with the request body and needed information was missing";
+        public const string FAILED_REQUEST_ERROR = "The request failed and was unable to be validated";
+    }
+}
