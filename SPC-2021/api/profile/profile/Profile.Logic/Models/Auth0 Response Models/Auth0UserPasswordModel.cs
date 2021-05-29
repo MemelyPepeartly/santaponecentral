37e713c080e906529cf99a3f83b1eb8c1f795a3c
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-29 02:26:52 -0600

    new common models

---
+++ SPC-2021/api/profile/profile/Profile.Logic/Models/Auth0 Response Models/Auth0UserPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profile.Logic.Models.Auth0_Response_Models
+{
+    public class Auth0UserPasswordModel
+    {
+        public string password { get; set; }
+    }
+}
