3f9c6b0ef3147b3f1dcbcef559442a296cd8efed
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-05-29 13:32:33 -0600

    Implimented shark tank service into message service

---
+++ SPC-2021/api/message/message/Message.Logic/Interfaces/ISharkTank.cs
@@ -0,0 +1,21 @@
+﻿using Message.Logic.Models.Auth0_Models;
+using Message.Logic.Models.Common_Models;
+using Message.Logic.Objects.Information_Objects;
+using System;
+using System.Threading.Tasks;
+
+namespace Message.Logic.Interfaces
+{
+    public interface ISharkTank
+    {
+        Task<Auth0UserInfoModel> GetAuthInfo(string authEmail);
+        Task<object> PostNewAuthUser();
+        Task<SharkTankValidationResponseModel> CheckIfValidRequest(SharkTankValidationModel requestModel);
+        Task<object> PutAuthEmail(Guid clientID);
+        Task<object> PutAuthName(Guid clientID);
+        Task<object> PutPasswordTicketRequest(Guid clientID);
+        Task<object> DeleteAuthUser(string authClientID);
+        Task<object> MakeNewSuccessLog(BaseClient requestingClient, string category);
+        Task<object> MakeNewFailureLog(BaseClient requestingClient, string category);
+    }
+}
---
+++ SPC-2021/api/message/message/Message.Logic/Models/Common Models/SharkTankValidationModel.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message.Logic.Models.Common_Models
+{
+    public class SharkTankValidationModel
+    {
+        public Guid requestorClientID { get; set; }
+        public List<Claim> requestorRoles { get; set; }
+        public string requestedObjectCategory { get; set; }
+        public Guid? requestedObjectID { get; set; }
+        public Method httpMethod { get; set; }
+    }
+}
