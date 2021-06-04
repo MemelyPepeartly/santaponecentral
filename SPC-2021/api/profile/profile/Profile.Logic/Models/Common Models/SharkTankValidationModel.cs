e08d1fe70614169dcd3b1fb00408b4e6c14723c7
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-06-03 21:34:29 -0600

    modified validation model

--- SPC-2021/api/profile/profile/Profile.Logic/Models/Common Models/SharkTankValidationModel.cs
+++ SPC-2021/api/profile/profile/Profile.Logic/Models/Common Models/SharkTankValidationModel.cs
@@ -10,10 +10,25 @@ namespace Profile.Logic.Models.Common_Models
 {
     public class SharkTankValidationModel
     {
+        /// <summary>
+        /// Client requesting the object
+        /// </summary>
         public Guid requestorClientID { get; set; }
+        /// <summary>
+        /// Roles the requestor has
+        /// </summary>
         public List<Claim> requestorRoles { get; set; }
+        /// <summary>
+        /// Object category for logging and sharkTank purposes
+        /// </summary>
         public string requestedObjectCategory { get; set; }
-        public Guid? requestedObjectID { get; set; }
+        /// <summary>
+        /// ID needed to validate against. Can be for any object, so long as it is a Guid
+        /// </summary>
+        public Guid? validationID { get; set; }
+        /// <summary>
+        /// HTTP method used in this transaction (GET, POST, PUT, DELETE)
+        /// </summary>
         public Method httpMethod { get; set; }
     }
 }
