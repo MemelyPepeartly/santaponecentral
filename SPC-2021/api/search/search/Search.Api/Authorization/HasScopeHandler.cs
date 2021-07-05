39d7a02efd4488ea778f8741ce8c4515355ab10b
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-07-05 15:53:42 -0600

    search cors and setup fixes

---
+++ SPC-2021/api/search/search/Search.Api/Authorization/HasScopeHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survey.Api.Authorization
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        {
+            // If user does not have the scope claim, get out of here
+            if (!context.User.HasClaim(c => c.Type == "permissions" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+
+            // Split the scopes string into an array
+            IEnumerable<PermissionClaim> scopes = context.User.Claims.Where(c => c.Type == "permissions").Select(c => new PermissionClaim{ Type = c.Type, Value = c.Value});
+
+            // Succeed if the scope array contains the required scope
+            if (scopes.Any(s => s.Value == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
