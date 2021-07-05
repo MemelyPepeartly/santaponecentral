3746adc667b922eeaa07d245b1b2973b1a7d251b
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-07-05 15:29:47 -0600

    permission scope handler

--- SPC-2021/api/survey/survey/Survey.Api/Authorization/HasScopeHandler.cs
+++ SPC-2021/api/survey/survey/Survey.Api/Authorization/HasScopeHandler.cs
@@ -11,14 +11,14 @@ namespace Survey.Api.Authorization
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
         {
             // If user does not have the scope claim, get out of here
-            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
+            if (!context.User.HasClaim(c => c.Type == "permissions" && c.Issuer == requirement.Issuer))
                 return Task.CompletedTask;
 
             // Split the scopes string into an array
-            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+            IEnumerable<PermissionClaim> scopes = context.User.Claims.Where(c => c.Type == "permissions").Select(c => new PermissionClaim{ Type = c.Type, Value = c.Value});
 
             // Succeed if the scope array contains the required scope
-            if (scopes.Any(s => s == requirement.Scope))
+            if (scopes.Any(s => s.Value == requirement.Scope))
                 context.Succeed(requirement);
 
             return Task.CompletedTask;
