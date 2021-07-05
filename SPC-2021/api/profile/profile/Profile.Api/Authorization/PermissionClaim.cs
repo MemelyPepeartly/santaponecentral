5d3ccd7ab2c9e03a17899a9a88efa33d943ab06e
SantaPoneCentralDev <santaponecentraldev@gmail.com>
2021-07-05 15:53:35 -0600

    profile cors and setup fixes

---
+++ SPC-2021/api/profile/profile/Profile.Api/Authorization/PermissionClaim.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Survey.Api.Authorization
+{
+    public class PermissionClaim
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
